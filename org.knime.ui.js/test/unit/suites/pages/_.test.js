90f0ee1a0bb275b15a3a22b9792fdc83a18e577a
Thomas Rosenau <thomas.rosenau@knime.com>
2020-07-29 11:31:17 +0200

    NXT-229: Move demo page to internal project
    NXT-229 (Split frontend code into dev and production code)

--- org.knime.ui.js/test/unit/suites/pages/_.test.js
+++
@@ -1,9 +0,0 @@
-import { shallowMount } from '@vue/test-utils';
-import page from '~/pages/_.vue';
-
-describe('index page', () => {
-    it('renders', () => {
-        let wrapper = shallowMount(page);
-        expect(wrapper.html()).toBeDefined();
-    });
-});
