4813fe811c74494f9ab6b084c8a8745595e00cad
Philipp Schimmelfennig <philipp.schimmelfennig@knime.com>
2020-07-30 14:05:59 +0200

    NXT-2: Add unit test to Kanvas
    NXT-2 (Display Workflow)

---
+++ org.knime.ui.js/test/unit/suites/components/Kanvas.test.js
@@ -0,0 +1,82 @@
+/* eslint-disable no-magic-numbers */
+import { shallowMount, createLocalVue } from '@vue/test-utils';
+import { mockVuexStore } from '~/test/unit/test-utils/mockVuexStore';
+import Vuex from 'vuex';
+
+import Kanvas from '~/components/Kanvas.vue';
+import Node from '~/components/Node';
+import Connector from '~/components/Connector.vue';
+
+const mockNode = ({ nodeID }) => ({
+    name: '',
+    nodeID,
+    uIInfo: { bounds: { x: 0, y: 0 } },
+    inPorts: [],
+    outPorts: [],
+    nodeType: '',
+    nodeAnnotation: { text: '' }
+});
+const mockConnector = ({ nr }) => ({
+    source: '',
+    dest: '',
+    sourcePort: nr,
+    destPort: 0
+});
+
+describe('Kanvas', () => {
+    let propsData, mocks, mount, wrapper, $store, workflow;
+
+    beforeAll(() => {
+        const localVue = createLocalVue();
+        localVue.use(Vuex);
+    });
+
+    beforeEach(() => {
+        wrapper = null;
+        propsData = {};
+        workflow = {
+            name: 'wf1',
+            nodes: {
+                'root:0': mockNode({ nodeID: 'root:0' }),
+                'root:1': mockNode({ nodeID: 'root:1' }),
+                'root:2': mockNode({ nodeID: 'root:2' })
+            },
+            connections: {
+                inA: mockConnector({ nr: 0 }),
+                outA: mockConnector({ nr: 1 }),
+                outB: mockConnector({ nr: 2 })
+            }
+        };
+        $store = mockVuexStore({
+            workflows: { state: { workflow } }
+        });
+
+        mocks = { $store };
+        mount = () => { wrapper = shallowMount(Kanvas, { propsData, mocks }); };
+    });
+
+
+    describe('renders default', () => {
+        beforeEach(() => {
+            mount();
+        });
+
+        it('heading', () => {
+            expect(wrapper.find('h3').text()).toBe('wf1 - 3 Nodes');
+        });
+
+        it('has portal for selection frames', () => {
+            expect(wrapper.find('portal-target[name="node-select"').exists()).toBe(true);
+        });
+
+        it('renders nodes', () => {
+            let props = wrapper.findAll(Node).wrappers.map(n => n.props());
+            expect(props).toStrictEqual(Object.values(workflow.nodes));
+        });
+
+        it('renders connectors', () => {
+            let props = wrapper.findAll(Connector).wrappers.map(c => c.props());
+            expect(props).toStrictEqual(Object.values(workflow.connections));
+        });
+    });
+});
