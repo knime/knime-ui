1ac70c6191174e1d1f76210154465035c3d91ee0
Thomas Rosenau <thomas.rosenau@knime.com>
2020-08-10 17:43:38 +0200

    NXT-248: Move api to public project
    NXT-248 (Internal-UI: get workflows directly from AP)

--- org.knime.ui.js/api/index.js
+++ org.knime.ui.js/api/index.js
@@ -1,3 +1,32 @@
-export const loadWorkflow = () => {
-    throw new Error('not implemented');
+// TODO: adjust implementations of all functions to final version of InitService (NXT-186)
+let RPCworkflows = [];
+
+const fetchFromAP = () => {
+    const req = {
+        jsonrpc: '2.0',
+        method: 'InitService.getWorkflows',
+        id: 0
+    };
+    let response = window.jsonrpc(JSON.stringify(req));
+
+    response = JSON.parse(response);
+    RPCworkflows = response.result.filter(Boolean).map(r => r.workflow);
+};
+
+export const loadWorkflow = (id) => {
+    let workflow;
+    workflow = RPCworkflows.find(workflow => workflow.name === id);
+
+    if (!workflow) { return Promise.resolve(null); }
+
+    return Promise.resolve({
+        ...workflow,
+        id,
+        name: id
+    });
+};
+
+export const getWorkflowIDs = () => {
+    fetchFromAP();
+    return Promise.resolve(RPCworkflows.map(workflow => workflow.name));
 };
