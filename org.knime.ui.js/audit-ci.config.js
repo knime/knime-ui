67231ab2da51489c0ce0546e0a715600eac7f1f2
Thomas Rosenau <thomas.rosenau@knime.com>
2020-08-24 10:40:57 +0200

    Fix npm audit issue 1548

--- org.knime.ui.js/audit-ci.config.js
+++ org.knime.ui.js/audit-ci.config.js
@@ -2,10 +2,14 @@
 // This allows to add exceptions for security issues found in npm modules that don't affect us.
 // Consider adding a comment with an explanation why each entry is added
 module.exports = {
+    // Enter exclusions below, for example
+    // '1548|nuxt>@nuxt/core>@nuxt/vue-renderer>vue-server-renderer>serialize-javascript'
+    // would whitelist issue https://www.npmjs.com/advisories/1548 in serialize-javascript
+    // for the given dependency path.
+    // Please refer to the https://www.npmjs.com/package/audit-ci README for details.
+    // Remember to add a comment explaining why the exclusion was added.
     allowlist: [
-        // Issue 1548 affects only the server-side renderer which is not used by this project
-        '1548|nuxt>@nuxt/core>@nuxt/server>@nuxt/vue-renderer>vue-server-renderer>serialize-javascript',
-        '1548|nuxt>@nuxt/core>@nuxt/vue-renderer>vue-server-renderer>serialize-javascript'
+        
     ],
     low: true,
     'package-manager': 'npm'
