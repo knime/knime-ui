3a9311ab1dce29220849b396d79d8f4c654fe45b
Thomas Rosenau <thomas.rosenau@knime.com>
2020-09-29 17:52:55 +0200

    NXT-241: Lintfix
    NXT-241 (Create color variables scheme for vue.js frontend)

--- org.knime.ui.js/test/unit/suites/components/Kanvas.test.js
+++ org.knime.ui.js/test/unit/suites/components/Kanvas.test.js
@@ -71,9 +71,15 @@ describe('Kanvas', () => {
                     'some id': nodeData
                 },
                 getters: {
-                    icon() { return ({ workflowId, nodeId }) => `data:image/${workflowId}-${nodeId}`; },
-                    name() { return ({ workflowId, nodeId }) => `name-${workflowId}-${nodeId}`; },
-                    type() { return ({ workflowId, nodeId }) => `type-${workflowId}-${nodeId}`; }
+                    icon() {
+                        return ({ workflowId, nodeId }) => `data:image/${workflowId}-${nodeId}`;
+                    },
+                    name() {
+                        return ({ workflowId, nodeId }) => `name-${workflowId}-${nodeId}`;
+                    },
+                    type() {
+                        return ({ workflowId, nodeId }) => `type-${workflowId}-${nodeId}`;
+                    }
                 }
             }
         });
@@ -122,8 +128,8 @@ describe('Kanvas', () => {
         const common = { bounds: { x: 0, y: 0, width: 42, height: 42 }, backgroundColor: '#fff', borderColor: '#000' };
         workflow.workflowAnnotations = [
             { ...common, id: 'back' },
-            { ...common,  id: 'middle'},
-            { ...common,  id: 'front'}
+            { ...common, id: 'middle' },
+            { ...common, id: 'front' }
         ];
         mount();
 
