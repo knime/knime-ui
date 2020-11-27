ae70c905ae92b522d2841fa4f7e3dd2bf554dc06
Thomas Rosenau <thomas.rosenau@knime.com>
2020-11-27 14:54:00 +0100

    NXT-19: Add test for production logger
    NXT-19 (Implement default data viewer table with “primitive” data)

---
+++ org.knime.ui.js/test/unit/suites/plugins/logger.test.js
@@ -0,0 +1,32 @@
+describe('logger', () => {
+    let originalConsola;
+
+    beforeEach(() => {
+        originalConsola = window.consola;
+        delete window.consola;
+    });
+
+    afterEach(() => {
+        window.consola = originalConsola;
+    });
+
+    it('defines a logger', () => {
+        require('~/plugins/logger');
+        [
+            'debug',
+            'error',
+            'fatal',
+            'info',
+            'log',
+            'ready',
+            'silent',
+            'start',
+            'success',
+            'trace',
+            'verbose',
+            'warn'
+        ].forEach(method => {
+            expect(window.consola[method]).toBeInstanceOf(Function);
+        });
+    });
+});
