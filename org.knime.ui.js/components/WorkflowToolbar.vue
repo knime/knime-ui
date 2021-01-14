2c6aa0c61448631a0db15d597c00a6e20449d0e1
Philipp Schimmelfennig <philipp.schimmelfennig@knime.com>
2021-01-14 12:35:23 +0100

    NXT-70: Refactor zoom menu
    NXT-70 (Zoom & Pan)

--- org.knime.ui.js/components/WorkflowToolbar.vue
+++ org.knime.ui.js/components/WorkflowToolbar.vue
@@ -5,11 +5,7 @@ import ToolbarButton from '~/components/ToolbarButton';
 import ExecuteAllIcon from '~/assets/execute-all.svg?inline';
 import CancelAllIcon from '~/assets/cancel-execution.svg?inline';
 import ResetAllIcon from '~/assets/reset-all.svg?inline';
-import FunctionButton from '~/webapps-common/ui/components/FunctionButton';
-import DropdownIcon from '~/webapps-common/ui/assets/img/icons/arrow-dropdown.svg?inline';
-import SubMenu from '~/webapps-common/ui/components/SubMenu';
-import Button from '~/webapps-common/ui/components/Button';
-import SplitButton from '~/webapps-common/ui/components/SplitButton';
+import ZoomMenu from '~/components/ZoomMenu';
 
 /**
  * A toolbar shown on top of a workflow canvas. Contains action buttons and breadcrumb.
@@ -21,8 +17,7 @@ export default {
         ExecuteAllIcon,
         CancelAllIcon,
         ResetAllIcon,
-        DropdownIcon,
-        SubMenu
+        ZoomMenu
     },
     data() {
         return {
@@ -35,35 +30,11 @@ export default {
             allowedActions: state => state.activeWorkflow?.allowedActions || {}
         }),
         ...mapGetters('workflow', ['activeWorkflowId']),
-        ...mapGetters('canvas', ['zoomFactor', 'fitToScreenZoomFactor']),
         hasBreadcrumb() {
             return this.workflow.parents?.length > 0;
-        },
-        zoomMenuItems() {
-            return [
-                {
-                    text: 'Zoom to fit',
-                    value: this.fitToScreenZoomFactor
-                }, {
-                    text: 'Zoom to 75%',
-                    value: 0.75
-                }, {
-                    text: 'Zoom to 100%',
-                    value: 1
-                }, {
-                    text: 'Zoom to 150%',
-                    value: 1.5
-                }
-            ];
         }
     },
-    mounted() {
-        this.$watch('zoomFactor', this.formatZoomInput, { immediate: true });
-    },
     methods: {
-        formatZoomInput() {
-            this.$refs.zoomInput.innerText = `${Math.round(this.zoomFactor * 100)}%`;
-        },
         onExecuteBtnClick() {
             this.$store.dispatch('workflow/executeNodes', {
                 nodeIds: [this.activeWorkflowId]
@@ -78,43 +49,6 @@ export default {
             this.$store.dispatch('workflow/resetNodes', {
                 nodeIds: [this.activeWorkflowId]
             });
-        },
-        onZoomInputEnter(e) {
-            console.log('enter pressed');
-            
-            let oldZoomFactor = this.zoomFactor;
-            let newZoomFactor = parseInt(e.target.innerText, 10) / 100;
-            
-            if (!isNaN(newZoomFactor)) {
-                this.$store.commit('canvas/setFactor', newZoomFactor);
-            }
-            
-            if (this.zoomFactor === oldZoomFactor) {
-                // zoom factor hasn't changed. Reset input field.
-                this.formatZoomInput();
-            }
-
-            e.stopPropagation();
-            e.preventDefault();
-            e.target.blur();
-        },
-        onZoomInputClick(e) {
-            e.target.focus();
-            document.execCommand('selectAll', false, null);
-            e.stopPropagation();
-        },
-        onZoomItemClick(e, item, id) {
-            let newZoomFactor = item.value;
-            this.$refs.zoomInput.blur();
-            this.$store.commit('canvas/setFactor', newZoomFactor);
-        },
-        onZoomMenuToggle(e) {
-            console.log('vlicked');
-            this.$refs.zoomInput.blur();
-        },
-        onZoomInputFocusOut(e) {
-            console.log('focus-lost');
-            this.formatZoomInput();
         }
     }
 };
@@ -151,22 +85,7 @@ export default {
       class="breadcrumb"
     />
 
-    <SubMenu
-      class="zoom"
-      :items="zoomMenuItems"
-      @item-click="onZoomItemClick"
-      @click.capture="onZoomMenuToggle"
-    >
-      <div
-        ref="zoomInput"
-        class="zoom-input"
-        contenteditable="true"
-        @click="onZoomInputClick"
-        @keydown.enter="onZoomInputEnter"
-        @focusout.stop="onZoomInputFocusOut"
-      />
-      <DropdownIcon />
-    </SubMenu>
+    <ZoomMenu class="zoommenu" />
   </div>
 </template>
 
@@ -187,33 +106,5 @@ export default {
   text-align: center;
   white-space: pre;
   flex: 1;
-}
-
-.zoom {
-  margin-left: auto;
-
-  & >>> .submenu-toggle {
-    padding: 0 13px 0 0;
-    align-items: center;
-
-    & svg {
-      height: 12px;
-      width: 12px;
-      stroke-width: calc(32px / 12);
-      margin-bottom: 1px;
-    }
-
-    &.expanded svg {
-      transform: scaleY(-1);
-    }
-
-    & .zoom-input {
-      cursor: text;
-      padding: 8px 4px 8px 16px;
-      font-size: 14px;
-      font-weight: 400;
-      margin-right: 0;
-    }
-  }
 }
 </style>
---
+++ org.knime.ui.js/components/ZoomMenu.vue
@@ -0,0 +1,109 @@
+<script>
+import { mapGetters } from 'vuex';
+import DropdownIcon from '~/webapps-common/ui/assets/img/icons/arrow-dropdown.svg?inline';
+import SubMenu from '~/webapps-common/ui/components/SubMenu';
+
+export default {
+    components: {
+        DropdownIcon,
+        SubMenu
+    },
+    computed: {
+        ...mapGetters('canvas', ['zoomFactor', 'fitToScreenZoomFactor']),
+        zoomMenuItems() {
+            return [
+                {
+                    text: 'Zoom to fit',
+                    value: this.fitToScreenZoomFactor
+                }, {
+                    text: 'Zoom to 75%',
+                    value: 0.75
+                }, {
+                    text: 'Zoom to 100%',
+                    value: 1
+                }, {
+                    text: 'Zoom to 150%',
+                    value: 1.5
+                }
+            ];
+        }
+    },
+    mounted() {
+        this.$watch('zoomFactor', this.formatZoomInput, { immediate: true });
+    },
+    methods: {
+        formatZoomInput() {
+            this.$refs.zoomInput.innerText = `${Math.round(this.zoomFactor * 100)}%`;
+        },
+        onZoomInputEnter(e) {
+            let newZoomFactor = parseInt(e.target.innerText, 10) / 100;
+            
+            if (!isNaN(newZoomFactor)) {
+                this.$store.commit('canvas/setFactor', newZoomFactor);
+            }
+
+            e.target.blur();
+        },
+        onZoomInputClick(e) {
+            e.target.focus();
+            document.execCommand('selectAll', false, null);
+        },
+        onZoomInputFocusOut(e) {
+            window.getSelection().removeAllRanges();
+            this.formatZoomInput();
+        },
+        onZoomItemClick(e, item, id) {
+            this.$store.commit('canvas/setFactor', item.value);
+            this.$refs.zoomInput.blur();
+        }
+    }
+};
+</script>
+
+<template>
+  <SubMenu
+    class="zoom"
+    :items="zoomMenuItems"
+    @item-click="onZoomItemClick"
+  >
+    <div
+      ref="zoomInput"
+      class="zoom-input"
+      contenteditable="true"
+      @click.stop="onZoomInputClick"
+      @keydown.enter.stop.prevent="onZoomInputEnter"
+      @focusout.stop="onZoomInputFocusOut"
+    />
+    <DropdownIcon />
+  </SubMenu>
+</template>
+
+<style lang="postcss" scoped>
+.zoom {
+  margin-left: auto;
+
+  & >>> .submenu-toggle {
+    padding: 0 13px 0 0;
+    align-items: center;
+
+    & svg {
+      height: 12px;
+      width: 12px;
+      stroke-width: calc(32px / 12);
+      margin-bottom: 1px;
+    }
+
+    &.expanded svg {
+      transform: scaleY(-1);
+    }
+
+    & .zoom-input {
+      cursor: text;
+      padding: 8px 4px 8px 16px;
+      font-size: 14px;
+      font-weight: 400;
+      margin-right: 0;
+    }
+  }
+}
+</style>
