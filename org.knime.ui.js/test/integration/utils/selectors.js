1ee34dda51adf13027437caccdcd24f0a5778443
Thomas Rosenau <thomas.rosenau@knime.com>
2021-04-12 15:05:15 +0200

    NXT-558: Fix node output tests
    NXT-558 (Fix initial automated integration tests)

--- org.knime.ui.js/test/integration/utils/selectors.js
+++ org.knime.ui.js/test/integration/utils/selectors.js
@@ -1,4 +1,5 @@
 module.exports = {
     cancelAllButton: '#toolbar button[title^="Cancel workflow execution"]',
-    executeAllButton: '#toolbar button[title^="Execute workflow"]'
+    executeAllButton: '#toolbar button[title^="Execute workflow"]',
+    executeSelectedButton: '#toolbar button[title^="Execute selected nodes"]'
 };
