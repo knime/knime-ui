1a0ca06ac4490c2491dee9fe8daeb77d7a35f70f
Philipp Schimmelfennig <philipp.schimmelfennig@knime.com>
2021-08-09 11:20:06 +0200

    NXT-111: Add connectNodes to API
    NXT-111 (Add node connectors)

--- org.knime.ui.js/api/index.js
+++ org.knime.ui.js/api/index.js
@@ -235,6 +235,27 @@ export const moveObjects = ({
 /**
  * @param { String } cfg.projectId
  * @param { String } cfg.workflowId
+ * @param { String } cfg.sourceNode node with outPort
+ * @param { String } cfg.destNode   node with inPort
+ * @param { String } cfg.sourcePort index of outPort
+ * @param { String } cfg.destPort   index of inPort
+ * @returns { Promise } Promise
+ */
+export const connectNodes = ({ projectId, workflowId, sourceNode, sourcePort, destNode, destPort }) => workflowCommand({
+    command: 'connect',
+    args: {
+        sourceNodeId: sourceNode,
+        sourcePortIdx: sourcePort,
+        destinationNodeId: destNode,
+        destinationPortIdx: destPort
+    },
+    projectId,
+    workflowId
+});
+
+/**
+ * @param { String } cfg.projectId
+ * @param { String } cfg.workflowId
  * @returns { Promise } Promise
  */
 export const undo = ({ projectId, workflowId }) => undoWorkflowCommand({
