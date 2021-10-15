0b44dc15f557a4da19293269d46d12f4c4245892
Philipp Schimmelfennig <philipp.schimmelfennig@knime.com>
2021-10-15 10:35:31 +0200

    NXT-747: Disable hotkeys on input elements
    NXT-747 (Using hotkeys doesn’t use the correct context in input fields — e.g. Zoom, (Search in Node Repo))

--- org.knime.ui.js/mixins/index.js
+++ org.knime.ui.js/mixins/index.js
@@ -2,3 +2,4 @@ export { connectorPosition } from '~/mixins/connectorPosition';
 export { portBar } from '~/mixins/portBar';
 export { tooltip } from '~/mixins/tooltip';
 export { snapConnector } from '~/mixins/snapConnector';
+export { hotKeys } from '~/mixins/hotKeys';
