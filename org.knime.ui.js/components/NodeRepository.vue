03cbe766be4df012062c368a591a6803af1b6c05
Karl Glatz <karl@glatz.biz>
2021-11-19 10:52:07 +0100

    NXTEXT-47: improve tags and search ui with simple reordering (tests failing!)
    NXTEXT-47 (Simple node search)

--- org.knime.ui.js/components/NodeRepository.vue
+++ org.knime.ui.js/components/NodeRepository.vue
@@ -2,16 +2,16 @@
 import { mapState, mapGetters } from 'vuex';
 
 import TagList from '~/webapps-common/ui/components/TagList';
-import Button from '~/webapps-common/ui/components/Button';
 import CloseIcon from '~/webapps-common/ui/assets/img/icons/close.svg?inline';
 import NodeRepositoryCategory from '~/components/NodeRepositoryCategory';
 import NodeSearcher from '~/components/NodeSearcher';
 import ScrollViewContainer from '~/components/ScrollViewContainer';
+import BreadcrumbEventBased from '~/components/BreadcrumbEventBased';
 
 export default {
     components: {
+        BreadcrumbEventBased,
         TagList,
-        Button,
         CloseIcon,
         NodeRepositoryCategory,
         NodeSearcher,
@@ -37,6 +37,14 @@ export default {
                 return [result];
             }
             return this.nodesPerCategory;
+        },
+        breadcrumbItems() {
+            let items = [{ text: 'Repository' }];
+            if (this.nodeSearching) {
+                items[0].id = 'clear';
+                items.push({ text: 'Results' });
+            }
+            return items;
         }
     },
     mounted() {
@@ -60,6 +68,12 @@ export default {
         clearNodeSearching() {
             this.$store.dispatch('nodeRepository/updateQuery', '');
         },
+        onBreadcrumbClick(e) {
+            if (e.id === 'clear') {
+                this.clearSelectedTags();
+                this.clearNodeSearching();
+            }
+        },
         lazyLoadCategories() {
             if (this.selectedTags.length === 0) {
                 this.$store.dispatch('nodeRepository/getAllNodes', true);
@@ -79,68 +93,51 @@ export default {
     @save-position="updateScrollPosition"
   >
     <div class="repo">
-      <h4
-        @click="clearNodeSearching"
-      >
-        Repository
-      </h4>
-      <span class="break" />
-      <NodeSearcher />
+      <div class="header">
+        <BreadcrumbEventBased
+          :items="breadcrumbItems"
+          class="repo-breadcrumb"
+          @click="onBreadcrumbClick"
+        />
+        <NodeSearcher />
+        <div class="tags">
+          <TagList
+            class="selected-tags"
+            :tags="selectedTags"
+            clickable
+            @click="deselectTag"
+          >
+            <CloseIcon slot="icon" />
+          </TagList>
+          <TagList
+            v-if="nodeSearching"
+            :tags="tags.filter((t) => !selectedTags.includes(t))"
+            clickable
+            @click="selectTag"
+          />
+        </div>
+        <span class="break full force" />
+      </div>
       <div
         v-if="nodeSearching && !nodes.length"
-        class="no-matching-search"
+        class="no-matching-search repo-content"
       >
         No node or component matching for: {{ query }}
       </div>
-      <div v-else>
-        <template v-if="nodeSearching">
-          <div class="tags">
-            <TagList
-              :tags="tags.filter((t) => !selectedTags.includes(t))"
-              clickable
-              @click="selectTag"
-            />
-          </div>
-          <span class="break full" />
+      <div
+        v-else
+        class="content"
+      >
+        <template v-for="(category, index) in categoriesDisplayed">
+          <NodeRepositoryCategory
+            :key="`tag-${index}`"
+            :category="category"
+          />
+          <span
+            :key="index"
+            class="break full"
+          />
         </template>
-
-        <div
-          v-if="selectedTags.length"
-          class="filter"
-        >
-          <div class="filter-tags">
-            Filter
-            <Button
-              compact
-              class="clear-button"
-              @click="clearSelectedTags"
-            >
-              Clear
-              <CloseIcon />
-            </Button>
-            <TagList
-              class="tag-list"
-              :tags="selectedTags"
-              clickable
-              @click="deselectTag"
-            >
-              <CloseIcon slot="icon" />
-            </TagList>
-          </div>
-        </div>
-        <div>
-          <template v-for="(category, index) in categoriesDisplayed">
-            <NodeRepositoryCategory
-              :key="`tag-${index}`"
-              :category="category"
-            />
-            <span
-              :key="index"
-              class="break full"
-            />
-          </template>
-        </div>
-        <div />
       </div>
     </div>
   </ScrollViewContainer>
@@ -148,7 +145,6 @@ export default {
 
 <style lang="postcss" scoped>
 .repo {
-  padding: 15px 20px;
   font-family: "Roboto Condensed", sans-serif;
   height: 100%;
   display: flex;
@@ -163,14 +159,20 @@ export default {
     height: 100%;
   }
 
-  & h4 {
+  & .repo-breadcrumb {
+    font-family: "Roboto Condensed", sans-serif;
     cursor: pointer;
     font-size: 18px;
     font-weight: 400;
-    margin: 16px 0px;
+    margin: 0;
+
+    & >>> a {
+      text-decoration: underline;
+    }
   }
 
-  & .break:not(:last-child) {
+  & .break:not(:last-child),
+  & .break.force {
     height: 1px;
     margin-bottom: 10px;
     padding-top: 1px;
@@ -185,6 +187,42 @@ export default {
     }
   }
 
+  & .header {
+    /* This can be removed if `scrollbar-gutter: stable;` is fully supported in all browsers */
+    width: 344px;
+    position: sticky;
+    background: var(--knime-gray-ultra-light);
+    z-index: 2;
+    top: 0;
+    padding: 15px 20px 0;
+  }
+
+  & .content {
+    padding: 0 20px 15px;
+  }
+
+  & .selected-tags {
+    & >>> .clickable {
+      color: var(--knime-white);
+      background-color: var(--knime-masala);
+      border-color: var(--knime-masala);
+
+      & svg {
+        stroke: var(--knime-white);
+      }
+
+      &:hover {
+        color: var(--knime-dove-gray);
+        background-color: transparent;
+        border-color: var(--knime-dove-gray);
+
+        & svg {
+          stroke: var(--knime-dove-gray);
+        }
+      }
+    }
+  }
+
   & .tags {
     display: flex;
     flex-wrap: wrap;
@@ -194,19 +232,6 @@ export default {
 
   & .filter {
     margin-bottom: 10px;
-
-    & .filter-tags {
-      margin-bottom: 12px;
-
-      & .clear-button {
-        float: right;
-        padding: 0;
-      }
-
-      & .tag-list {
-        margin-top: 8px;
-      }
-    }
   }
 }
 </style>
