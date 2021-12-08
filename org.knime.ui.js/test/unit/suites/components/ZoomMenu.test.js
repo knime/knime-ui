742ff3e789d211c96cf175f89128ff1acc853cfe
Philipp Schimmelfennig <philipp.schimmelfennig@knime.com>
2021-12-08 13:53:39 +0100

    NXT-771: Refactor Unit test

--- org.knime.ui.js/test/unit/suites/components/ZoomMenu.test.js
+++ org.knime.ui.js/test/unit/suites/components/ZoomMenu.test.js
@@ -6,128 +6,151 @@ import { mockVuexStore } from '~/test/unit/test-utils';
 import ZoomMenu from '~/components/ZoomMenu';
 
 describe('ZoomMenu', () => {
-    let $store, zoomFactor;
+    let doMount, $store, zoomFactor, wrapper, storeConfig;
 
     beforeAll(() => {
         const localVue = createLocalVue();
         localVue.use(Vuex);
     });
 
-    zoomFactor = 1;
+    beforeEach(() => {
+        zoomFactor = 1;
 
-    let doMount = () => {
-        $store = mockVuexStore({
-            canvas: {
-                state: {
-                    zoomFactor
-                },
-                mutations: {
-                    setFactor(state, factor) {
-                        state.zoomFactor = factor;
+        doMount = () => {
+            storeConfig = {
+                canvas: {
+                    state: {
+                        zoomFactor
+                    },
+                    mutations: {
+                        setFactor(state, factor) {
+                            state.zoomFactor = factor;
+                        }
+                    },
+                    getters: {
+                        fitToScreenZoomFactor() {
+                            return 5;
+                        }
+                    },
+                    actions: {
+                        zoomCentered: jest.fn()
                     }
                 },
-                getters: {
-                    fitToScreenZoomFactor() {
-                        return 5;
+                userActions: {
+                    getters: {
+                        zoomActionItems() {
+                            return [{
+                                text: '100 %',
+                                storeAction: 'testZoomTo100',
+                                storeActionParams: []
+                            }];
+                        }
                     }
-                },
-                actions: {
-                    zoomCentered: jest.fn()
                 }
-            },
-            userActions: {
-                getters: {
-                    zoomActionItems() {
-                        return [{
-                            text: '100 %',
-                            storeAction: 'testZoomTo100',
-                            storeActionParams: []
-                        }];
-                    }
+            };
+
+            $store = mockVuexStore(storeConfig);
+            wrapper = mount(ZoomMenu, {
+                mocks: {
+                    $store
                 }
-            }
-        });
-        return mount(ZoomMenu, {
-            mocks: {
-                $store
-            }
-        });
-    };
+            });
+        };
+    });
+
 
     it('renders', () => {
-        let wrapper = doMount();
+        doMount();
         expect(wrapper.html()).toBeTruthy();
     });
 
-    it('shows current zoom level', () => {
-        zoomFactor = 0.53;
-        let wrapper = doMount();
-        expect(wrapper.find('.zoom-input').element.value).toBe('53%');
-    });
+    describe('zoom value input', () => {
+        it('shows current zoom level', () => {
+            zoomFactor = 0.53;
+            doMount();
 
-    it('parses input with % sign', async () => {
-        zoomFactor = 0.53;
-        let wrapper = doMount();
-        let input = wrapper.find('.zoom-input');
-        input.element.value = '33%';
-        await input.trigger('keydown', { keyCode: 13 });
-        expect(input.element.value).toBe('33%');
-    });
+            expect(wrapper.find('.zoom-input').element.value).toBe('53%');
+        });
 
-    it('parses input without % sign', async () => {
-        zoomFactor = 0.53;
-        let wrapper = doMount();
-        let input = wrapper.find('.zoom-input');
-        input.element.value = '44';
-        await input.trigger('keydown', { keyCode: 13 });
-        expect(input.element.value).toBe('44%');
-    });
+        it('selects all text of input on click', () => {
+            zoomFactor = 0.63;
+            doMount();
 
-    it('ignores invalid input', async () => {
-        zoomFactor = 0.63;
-        let wrapper = doMount();
-        let input = wrapper.find('.zoom-input');
-        input.element.value = 'asdf';
-        await input.trigger('keydown', { keyCode: 13 });
-        expect(input.element.value).toBe('63%');
-    });
+            let input = wrapper.find('.zoom-input');
+            input.element.select = jest.fn();
+            input.trigger('click');
 
-    it('ignores any input on focus out', () => {
-        zoomFactor = 0.63;
-        let wrapper = doMount();
-        let input = wrapper.find('.zoom-input');
-        input.element.value = '99';
-        input.trigger('focusout');
-        expect(input.element.value).toBe('63%');
-    });
+            expect(input.element.select).toHaveBeenCalled();
+        });
 
+        it('parses input with % sign', async () => {
+            zoomFactor = 0.53;
+            doMount();
 
-    it('selects all text of input on click', () => {
-        zoomFactor = 0.63;
-        let wrapper = doMount();
-        let input = wrapper.find('.zoom-input');
-        input.element.select = jest.fn();
-        input.trigger('click');
-        expect(input.element.select).toHaveBeenCalled();
+            let input = wrapper.find('.zoom-input');
+            input.element.value = '33%';
+            await input.trigger('keydown', { keyCode: 13 });
+
+            expect(input.element.value).toBe('33%');
+        });
+
+        it('parses input without % sign', async () => {
+            zoomFactor = 0.53;
+            doMount();
+
+            let input = wrapper.find('.zoom-input');
+            input.element.value = '44';
+            await input.trigger('keydown', { keyCode: 13 });
+
+            expect(input.element.value).toBe('44%');
+        });
+
+        it('ignores invalid input', async () => {
+            zoomFactor = 0.63;
+            doMount();
+
+            let input = wrapper.find('.zoom-input');
+            input.element.value = 'asdf';
+            await input.trigger('keydown', { keyCode: 13 });
+
+            expect(input.element.value).toBe('63%');
+        });
+
+        it('ignores any input on focus out', () => {
+            zoomFactor = 0.63;
+            doMount();
+
+            let input = wrapper.find('.zoom-input');
+            input.element.value = '99';
+            input.trigger('focusout');
+
+            expect(input.element.value).toBe('63%');
+        });
     });
 
     it('dispatches action on click of item', () => {
         zoomFactor = 0.63;
-        let wrapper = doMount();
+        doMount();
         $store.dispatch = jest.fn();
+
         let li = wrapper.find('li');
         li.trigger('click');
+
         expect($store.dispatch).toHaveBeenCalledWith('testZoomTo100');
     });
 
     it('zooms in and out on mousewheel', async () => {
         zoomFactor = 0.63;
-        let wrapper = doMount();
+        doMount();
         $store.dispatch = jest.fn();
+
         let input = wrapper.find('.zoom-input');
         await input.trigger('wheel', { deltaY: 1 });
+
         expect($store.dispatch).toHaveBeenCalledWith('canvas/zoomCentered', -1);
+
         await input.trigger('wheel', { deltaY: -1 });
+
         expect($store.dispatch).toHaveBeenCalledWith('canvas/zoomCentered', 1);
     });
 });
