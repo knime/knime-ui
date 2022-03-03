e200d84bf05559e58a39a6262ba9612d6f2a877b
peter.schramm@knime.com <peter.schramm@knime.com>
2022-03-03 11:01:20 +0100

    NXT-940: Swap hotkeys

--- org.knime.ui.js/commands/executionCommands.js
+++ org.knime.ui.js/commands/executionCommands.js
@@ -24,7 +24,7 @@ export default {
     cancelAll: {
         text: 'Cancel all',
         title: 'Cancel workflow execution',
-        hotkey: ['Shift', 'F9'],
+        hotkey: ['Shift', 'F8'],
         icon: CancelAllIcon,
         execute:
             ({ $store }) => $store.dispatch('workflow/cancelNodeExecution', 'all'),
@@ -34,7 +34,7 @@ export default {
     resetAll: {
         text: 'Reset all',
         title: 'Reset executed nodes',
-        hotkey: ['Shift', 'F8'],
+        hotkey: ['Shift', 'F9'],
         icon: ResetAllIcon,
         execute:
             ({ $store }) => $store.dispatch('workflow/resetNodes', 'all'),
