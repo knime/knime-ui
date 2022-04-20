263d935c03491a8a55fd28ce0b6f286962b33742
Karl Glatz <karl@glatz.biz>
2022-04-20 09:10:37 +0200

    NXT-898: add test for ContextMenu items visible states
    NXT-898 (Write unit tests for commands)

--- org.knime.ui.js/test/unit/suites/components/ContextMenu.test.js
+++ org.knime.ui.js/test/unit/suites/components/ContextMenu.test.js
@@ -41,8 +41,15 @@ describe('ContextMenu.vue', () => {
 
         doMount = () => {
             $store = mockVuexStore(storeConfig);
-            mocks = { $store, $commands };
-            wrapper = mount(ContextMenu, { propsData, mocks });
+            mocks = {
+                $store,
+                $commands
+            };
+            wrapper = mount(
+                ContextMenu, {
+                    propsData,
+                    mocks
+                });
         };
     });
 
@@ -57,16 +64,22 @@ describe('ContextMenu.vue', () => {
     it('shows menu', async () => {
         doMount();
         expect(wrapper.findComponent(FloatingMenu).classes()).not.toContain('isVisible');
-        
-        wrapper.vm.show({ pageX: 0, pageY: 0 });
+
+        wrapper.vm.show({
+            pageX: 0,
+            pageY: 0
+        });
         await Vue.nextTick();
-        
+
         expect(wrapper.findComponent(FloatingMenu).classes()).toContain('isVisible');
     });
 
     it('uses right format for menuItems for FloatingMenu', async () => {
         doMount();
-        wrapper.vm.show({ pageX: 0, pageY: 0 });
+        wrapper.vm.show({
+            pageX: 0,
+            pageY: 0
+        });
 
         wrapper.vm.visibleCommands = ['undo'];
         await Vue.nextTick();
@@ -87,5 +100,132 @@ describe('ContextMenu.vue', () => {
         doMount();
         wrapper.findComponent(FloatingMenu).vm.$emit('item-click', null, { name: 'command' });
         expect($commands.dispatch).toHaveBeenCalledWith('command');
+    });
+
+    describe('visibility of menu items', () => {
+        it('shows correct menu items if nothing is selected', () => {
+            doMount();
+            wrapper.vm.setMenuItems();
+            expect(wrapper.vm.visibleCommands).toStrictEqual(['executeAll', 'cancelAll', 'resetAll']);
+        });
+
+        it('shows correct menu items if one node is selected', () => {
+            let node = {
+                id: 'root:0',
+                allowedActions: {}
+            };
+            storeConfig.selection.getters.selectedNodes = () => [node];
+            storeConfig.selection.getters.singleSelectedNode = () => node;
+            doMount();
+            wrapper.vm.setMenuItems();
+            expect(wrapper.vm.visibleCommands).toStrictEqual([
+                'executeSelected',
+                'cancelSelected',
+                'resetSelected',
+                'configureNode',
+                'deleteSelected'
+            ]);
+        });
+
+        it('shows correct menu items if selected node has loopInfo', () => {
+            let node = {
+                id: 'root:0',
+                allowedActions: {},
+                loopInfo: { allowedActions: {} }
+            };
+            storeConfig.selection.getters.selectedNodes = () => [node];
+            storeConfig.selection.getters.singleSelectedNode = () => node;
+            doMount();
+            wrapper.vm.setMenuItems();
+            expect(wrapper.vm.visibleCommands).toStrictEqual([
+                'executeSelected',
+                'resumeLoopExecution',
+                'pauseLoopExecution',
+                'stepLoopExecution',
+                'cancelSelected',
+                'resetSelected',
+                'configureNode',
+                'deleteSelected'
+            ]);
+        });
+
+        it('shows correct menu items if selected node can open view', () => {
+            let node = {
+                id: 'root:0',
+                allowedActions: {
+                    canOpenLegacyFlowVariableDialog: true,
+                    canOpenView: true
+                }
+            };
+            storeConfig.selection.getters.selectedNodes = () => [node];
+            storeConfig.selection.getters.singleSelectedNode = () => node;
+            doMount();
+            wrapper.vm.setMenuItems();
+            expect(wrapper.vm.visibleCommands).toStrictEqual([
+                'executeSelected',
+                'cancelSelected',
+                'resetSelected',
+                'configureNode',
+                'configureFlowVariables',
+                'openView',
+                'deleteSelected'
+            ]);
+        });
+
+        it('shows correct menu items for multiple selected nodes', () => {
+            let node = {
+                id: 'root:0',
+                allowedActions: {
+                    canOpenLegacyFlowVariableDialog: true,
+                    canOpenView: true,
+                    loopInfo: { allowedActions: {} }
+                }
+            };
+            let node2 = {
+                id: 'root:1',
+                allowedActions: {
+                    canOpenLegacyFlowVariableDialog: true,
+                    canOpenView: true,
+                    loopInfo: { allowedActions: {} }
+                }
+            };
+            storeConfig.selection.getters.selectedNodes = () => [node, node2];
+            storeConfig.selection.getters.singleSelectedNode = () => null;
+            doMount();
+            wrapper.vm.setMenuItems();
+            expect(wrapper.vm.visibleCommands).toStrictEqual([
+                'executeSelected',
+                'cancelSelected',
+                'resetSelected',
+                'deleteSelected'
+            ]);
+        });
+
+        it('shows correct menu items for multiple selected connections', () => {
+            let conn = {
+                id: 'conn1'
+            };
+            let conn2 = {
+                id: 'conn2'
+            };
+            storeConfig.selection.getters.selectedConnections = () => [conn, conn2];
+            doMount();
+            wrapper.vm.setMenuItems();
+            expect(wrapper.vm.visibleCommands).toStrictEqual([
+                'deleteSelected'
+            ]);
+        });
+
+        it('shows correct menu items for single selected connections', () => {
+            let conn = {
+                id: 'conn1'
+            };
+            storeConfig.selection.getters.selectedConnections = () => [conn];
+            doMount();
+            wrapper.vm.setMenuItems();
+            expect(wrapper.vm.visibleCommands).toStrictEqual([
+                'deleteSelected'
+            ]);
+        });
     });
 });
