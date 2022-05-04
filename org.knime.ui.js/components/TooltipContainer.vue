069c5271bb96e5b9c0ba8ab8513df3f4eab6e897
Philipp Schimmelfennig <philipp.schimmelfennig@knime.com>
2022-05-04 15:17:14 +0200

    NXT-1065: Delay Tooltip rendering
    NXT-1065 (Tooltips block UI before they are shown)

--- org.knime.ui.js/components/TooltipContainer.vue
+++ org.knime.ui.js/components/TooltipContainer.vue
@@ -124,8 +124,7 @@ export default {
   height: 0;
 
   & .tooltip-enter-active {
-    /* delay entering of tooltip by 0.75 seconds */
-    transition: opacity 150ms 0.75s ease;
+    transition: opacity 150ms ease;
   }
 
   & .tooltip-leave-active {
