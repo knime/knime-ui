723e3edc68dfd46e2102ed558552e24dfd5b79b2
Philipp Schimmelfennig <philipp.schimmelfennig@knime.com>
2022-06-13 22:53:51 +0200

    NXT-1007: Split workflow execution test

--- org.knime.ui.js/test/unit/suites/store/workflow/workflow.test.js
+++ org.knime.ui.js/test/unit/suites/store/workflow/workflow.test.js
@@ -185,66 +185,6 @@ describe('workflow store', () => {
             expect(removeEventListenerMock).not.toHaveBeenCalled();
         });
 
-        // describe used for debugging
-        describe('each', () => {
-            it.each([
-                ['executeNodes', 'execute'],
-                ['cancelNodeExecution', 'cancel'],
-                ['resetNodes', 'reset']
-            ])('passes %s to API', async (fn, action) => {
-                let mock = jest.fn();
-                let apiMocks = { changeNodeState: mock };
-                await loadStore({ apiMocks });
-                store.commit('workflow/setActiveWorkflow', { projectId: 'foo', info: {} });
-                store.dispatch(`workflow/${fn}`, ['x', 'y']);
-
-                expect(mock).toHaveBeenCalledWith({
-                    nodeIds: ['x', 'y'], projectId: 'foo', action, workflowId: 'root'
-                });
-            });
-
-            it.each([
-                ['pauseLoopExecution', 'pause'],
-                ['resumeLoopExecution', 'resume'],
-                ['stepLoopExecution', 'step']
-            ])('passes %s to API', async (fn, action) => {
-                let mock = jest.fn();
-                let apiMocks = { changeLoopState: mock };
-                await loadStore({ apiMocks });
-                store.commit('workflow/setActiveWorkflow', { projectId: 'foo', info: {} });
-
-                store.dispatch(`workflow/${fn}`, 'node x');
-
-                expect(mock).toHaveBeenCalledWith({ nodeId: 'node x', projectId: 'foo', action, workflowId: 'root' });
-            });
-        });
-
-        test('overloaded changeNodeState', async () => {
-            let mock = jest.fn();
-            let apiMocks = { changeNodeState: mock };
-            await loadStore({ apiMocks });
-            store.commit('workflow/setActiveWorkflow', {
-                projectId: 'foo',
-                info: {},
-                nodes: {
-                    'root:1': { id: 'root:1' },
-                    'root:2': { id: 'root:2' }
-                }
-            });
-
-            store.dispatch(`workflow/changeNodeState`, { nodes: 'all' });
-            expect(mock).toHaveBeenLastCalledWith({ projectId: 'foo', workflowId: 'root' });
-
-            store.dispatch('selection/selectAllNodes');
-            store.dispatch(`workflow/changeNodeState`, { nodes: 'selected' });
-            expect(mock).toHaveBeenLastCalledWith({
-                nodeIds: ['root:1', 'root:2'], projectId: 'foo', workflowId: 'root'
-            });
-
-            store.dispatch(`workflow/changeNodeState`, { nodes: ['root:2'] });
-            expect(mock).toHaveBeenLastCalledWith({ nodeIds: ['root:2'], projectId: 'foo', workflowId: 'root' });
-        });
-
         it.each([
             ['undo'],
             ['redo']
