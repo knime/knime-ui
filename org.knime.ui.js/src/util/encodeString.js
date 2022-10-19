305867bd8a630f782d127d699fa7eff16b2f3594
Helian Rivera <helian.rivera@knime.com>
2022-10-19 16:57:50 +0200

    NXT-1268: fix error enconding workflow name with Unicode chars
    NXT-1268 (Cannot enter container node if parent workflow name contains Unicode EN DASH character)

---
+++ org.knime.ui.js/src/util/encodeString.js
@@ -0,0 +1,15 @@
+export const encodeString = (stringValue) => {
+    // See:
+    // https://stackoverflow.com/questions/30106476/using-javascripts-atob-to-decode-base64-doesnt-properly-decode-utf-8-strings
+    // convert a Unicode string to a string in which
+    // each 16-bit unit occupies only one byte
+    const toBinary = (string) => {
+        const codeUnits = new Uint16Array(string.length);
+        for (let i = 0; i < codeUnits.length; i++) {
+            codeUnits[i] = string.charCodeAt(i);
+        }
+        return btoa(String.fromCharCode(...new Uint8Array(codeUnits.buffer)));
+    };
+
+    return toBinary(stringValue);
+};
