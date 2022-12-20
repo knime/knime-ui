cc0b7609a37e7177769349be8571e594ed9a5a07
Helian Rivera <helian.rivera@knime.com>
2022-12-20 21:34:48 +0100

    NXT-1369: export routes for usage in ui-internal app
    NXT-1369 (Info page for “About”, “Install extensions“, …)

--- org.knime.ui.js/src/router/index.js
+++ org.knime.ui.js/src/router/index.js
@@ -21,35 +21,41 @@ export const APP_ROUTES = {
     }
 };
 
+export const routes = Object.values(APP_ROUTES);
+
 const router = new VueRouter({
     mode: 'history',
-    routes: Object.values(APP_ROUTES)
+    routes
 });
 
-// See: https://github.com/vuejs/vue-router/issues/2932#issuecomment-533453711
-const originalPush = router.push;
-router.push = function push(location, onResolve, onReject) {
-    if (onResolve || onReject) {
-        return originalPush.call(this, location, onResolve, onReject);
-    }
-
-    const ERROR_TYPES = {
-        '2': 'redirected',
-        '4': 'aborted',
-        '8': 'cancelled',
-        '16': 'duplicated'
-    };
-
-    return originalPush.call(this, location).catch((err) => {
-        if (!ERROR_TYPES[err.type]) {
-            // If there really is an error, throw it
-            return Promise.reject(err);
+export const muteRouterExceptions = (_router) => {
+    // See: https://github.com/vuejs/vue-router/issues/2932#issuecomment-533453711
+    const originalPush = _router.push;
+    _router.push = function push(location, onResolve, onReject) {
+        if (onResolve || onReject) {
+            return originalPush.call(this, location, onResolve, onReject);
         }
 
-        // Otherwise resolve to false to indicate the original push call didn't go to its original
-        // destination.
-        return Promise.resolve(false);
-    });
+        const ERROR_TYPES = {
+            '2': 'redirected',
+            '4': 'aborted',
+            '8': 'cancelled',
+            '16': 'duplicated'
+        };
+
+        return originalPush.call(this, location).catch((error) => {
+            if (!ERROR_TYPES[error.type]) {
+                // If there really is an error, throw it
+                return Promise.reject(error);
+            }
+
+            // Otherwise resolve to false to indicate the original push call didn't go to its original
+            // destination.
+            return Promise.resolve(false);
+        });
+    };
 };
+
+muteRouterExceptions(router);
 
 export default router;
