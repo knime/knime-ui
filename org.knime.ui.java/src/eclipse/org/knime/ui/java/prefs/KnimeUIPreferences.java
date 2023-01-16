27d47fa72f273686dafc800fa6392c88493282e3
Benjamin Wilhelm <benjamin.wilhelm@knime.com>
2023-01-16 17:05:22 +0100

    NXT-1402: Add Modern UI preference page
    NXT-1402 (Node repo based on a set of nodes or all nodes)

---
+++ org.knime.ui.java/src/eclipse/org/knime/ui/java/prefs/KnimeUIPreferences.java
@@ -0,0 +1,124 @@
+/*
+ * ------------------------------------------------------------------------
+ *
+ *  Copyright by KNIME AG, Zurich, Switzerland
+ *  Website: http://www.knime.com; Email: contact@knime.com
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License, Version 3, as
+ *  published by the Free Software Foundation.
+ *
+ *  This program is distributed in the hope that it will be useful, but
+ *  WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, see <http://www.gnu.org/licenses>.
+ *
+ *  Additional permission under GNU GPL version 3 section 7:
+ *
+ *  KNIME interoperates with ECLIPSE solely via ECLIPSE's plug-in APIs.
+ *  Hence, KNIME and ECLIPSE are both independent programs and are not
+ *  derived from each other. Should, however, the interpretation of the
+ *  GNU GPL Version 3 ("License") under any applicable laws result in
+ *  KNIME and ECLIPSE being a combined program, KNIME AG herewith grants
+ *  you the additional permission to use and propagate KNIME together with
+ *  ECLIPSE with only the license terms in place for ECLIPSE applying to
+ *  ECLIPSE and the GNU GPL Version 3 applying for KNIME, provided the
+ *  license terms of ECLIPSE themselves allow for the respective use and
+ *  propagation of ECLIPSE together with KNIME.
+ *
+ *  Additional permission relating to nodes for KNIME that extend the Node
+ *  Extension (and in particular that are based on subclasses of NodeModel,
+ *  NodeDialog, and NodeView) and that only interoperate with KNIME through
+ *  standard APIs ("Nodes"):
+ *  Nodes are deemed to be separate and independent programs and to not be
+ *  covered works.  Notwithstanding anything to the contrary in the
+ *  License, the License does not apply to Nodes, you are not required to
+ *  license Nodes under the License, and you are granted a license to
+ *  prepare and propagate Nodes, in each case even if such Nodes are
+ *  propagated with or for interoperation with KNIME.  The owner of a Node
+ *  may freely choose the license terms applicable to such Node, including
+ *  when such Node is propagated with or for interoperation with KNIME.
+ * ---------------------------------------------------------------------
+ *
+ * History
+ *   Jan 13, 2023 (benjamin): created
+ */
+package org.knime.ui.java.prefs;
+
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.function.BiConsumer;
+
+import org.eclipse.core.runtime.preferences.InstanceScope;
+import org.eclipse.jface.preference.IPreferenceStore;
+import org.eclipse.ui.preferences.ScopedPreferenceStore;
+import org.knime.ui.java.UIPlugin;
+
+/**
+ * The preference of the modern UI.
+ *
+ * @author Benjamin Wilhelm, KNIME GmbH, Berlin, Germany
+ */
+public final class KnimeUIPreferences {
+
+    private static final List<BiConsumer<String, String>> NODE_REPO_FILTER_CHANGE_LISTENERS =
+        Collections.synchronizedList(new LinkedList<>());
+
+    static final String NODE_REPO_FILTER_PREF_KEY = "nodeRepositoryFilterId";
+
+    static final IPreferenceStore PREF_STORE =
+        new ScopedPreferenceStore(InstanceScope.INSTANCE, UIPlugin.getContext().getBundle().getSymbolicName());
+
+    static {
+        PREF_STORE.addPropertyChangeListener(e -> {
+            if (NODE_REPO_FILTER_PREF_KEY.equals(e.getProperty())) {
+                for (var l : NODE_REPO_FILTER_CHANGE_LISTENERS) {
+                    l.accept((String)e.getOldValue(), (String)e.getNewValue());
+                }
+            }
+        });
+    }
+
+    private KnimeUIPreferences() {
+        // Utility class
+    }
+
+    /** The identifier for a no node repository filter. The node repository will show all nodes. */
+    public static final String NODE_REPO_FILTER_NONE_ID = "none";
+
+    /**
+     * The identifier for the spreadsheet collection node repository filter. The node repository will only show nodes
+     * that are useful for spreadsheet users.
+     */
+    static final String NODE_REPO_FILTER_SPREADSHEET_ID = "spreadsheet";
+
+    /**
+     * @return the identifier of the node repository filter that is active. {@link #NODE_REPO_FILTER_NONE_ID} if no
+     *         filter is active.
+     */
+    public static String getNodeRepoFilter() {
+        return PREF_STORE.getString(NODE_REPO_FILTER_PREF_KEY);
+    }
+
+    /**
+     * Add a listener that is called whenever the node repository filter setting changes.
+     *
+     * @param listener the listener that is called with the old value and the new value.
+     */
+    public static void addNodeRepoFilterChangeListener(final BiConsumer<String, String> listener) {
+        NODE_REPO_FILTER_CHANGE_LISTENERS.add(listener);
+    }
+
+    /**
+     * Remove a listener that was added with {@link #addNodeRepoFilterChangeListener(BiConsumer)}.
+     *
+     * @param listener the listener
+     */
+    public static void removeNodeRepoFilterChangeListener(final BiConsumer<String, String> listener) {
+        NODE_REPO_FILTER_CHANGE_LISTENERS.remove(listener);
+    }
+}
