873a9efae259eb071ba8dfa15cdf54b56f1f6b90
Karl Glatz <karl@glatz.biz>
2023-03-06 14:04:14 +0100

    NXT-1262: refactor node search into store submodule
    NXT-1262 (Search in the quick add node dialog)

---
+++ org.knime.ui.js/src/store/common/nodeSearch.js
@@ -0,0 +1,265 @@
+import { searchNodes } from '@api';
+import { toNodeWithFullPorts } from '@/util/portDataMapper';
+
+/**
+ * This store is not instantiated by Vuex but used by other stores.
+ */
+
+const nodeSearchPageSize = 100;
+
+export const state = {
+    /* search results */
+    selectedTags: [],
+
+    query: '',
+
+    searchScrollPosition: 0,
+    isShowingBottomNodes: false,
+
+    topNodes: null,
+    totalNumTopNodes: 0,
+    topNodeSearchPage: 0,
+    topNodesTags: [],
+
+    bottomNodes: null,
+    totalNumBottomNodes: 0,
+    bottomNodeSearchPage: 0,
+    bottomNodesTags: []
+};
+
+export const mutations = {
+    setTopNodeSearchPage(state, pageNumber) {
+        state.topNodeSearchPage = pageNumber;
+    },
+
+    setTotalNumTopNodes(state, totalNumTopNodes) {
+        state.totalNumTopNodes = totalNumTopNodes;
+    },
+
+    addTopNodes(state, topNodes) {
+        let existingNodeIds = state.topNodes.map(node => node.id);
+        let newNodes = topNodes.filter(node => !existingNodeIds.includes(node.id));
+        state.topNodes.push(...newNodes);
+    },
+
+    setTopNodes(state, topNodes) {
+        state.topNodes = topNodes;
+    },
+
+    setTopNodesTags(state, topNodesTags) {
+        state.topNodesTags = topNodesTags;
+    },
+
+    setBottomNodeSearchPage(state, pageNumber) {
+        state.bottomNodeSearchPage = pageNumber;
+    },
+
+    setTotalNumBottomNodes(state, totalNumBottomNodes) {
+        state.totalNumBottomNodes = totalNumBottomNodes;
+    },
+
+    setBottomNodes(state, bottomNodes) {
+        state.bottomNodes = bottomNodes;
+    },
+
+    setBottomNodesTags(state, bottomNodesTags) {
+        state.bottomNodesTags = bottomNodesTags;
+    },
+
+    setSelectedTags(state, selectedTags) {
+        state.selectedTags = selectedTags;
+        state.searchScrollPosition = 0;
+    },
+
+    setQuery(state, value) {
+        state.query = value;
+        state.searchScrollPosition = 0;
+    },
+
+    setSearchScrollPosition(state, value) {
+        state.searchScrollPosition = value;
+    },
+
+    setShowingBottomNodes(state, value) {
+        state.isShowingBottomNodes = value;
+    }
+};
+
+export const actions = {
+    /**
+     * Fetch nodes. Used for initial data retrieval, but also for searching via query and/or tag filters.
+     *
+     * @param {*} context - Vuex context.
+     * @param {Boolean} append - if the results should be added to the current nodes (for pagination) or if the state
+     *      should be cleared (for a new search).
+     * @param {Boolean} bottom - if search should be for additional nodes that are displayed on the bottom
+     * @returns {Promise}
+     */
+    async searchNodes({
+        commit,
+        state,
+        dispatch,
+        getters,
+        rootState
+    }, {
+        append = false,
+        bottom = false
+    } = {}) {
+        // only do request if we search for something
+        if (!getters.hasSearchParams) {
+            // clear old results
+            dispatch('clearSearchResults');
+            return;
+        }
+        const prefix = bottom ? 'Bottom' : 'Top';
+        const currentPage = () => bottom ? state.bottomNodeSearchPage : state.topNodeSearchPage;
+
+        if (append) {
+            commit(`set${prefix}NodeSearchPage`, currentPage() + 1);
+        } else {
+            commit(`set${prefix}NodeSearchPage`, 0);
+        }
+
+        const {
+            nodes,
+            totalNumNodes,
+            tags
+        } = await searchNodes({
+            query: state.query,
+            tags: state.selectedTags,
+            allTagsMatch: true,
+            nodeOffset: currentPage() * nodeSearchPageSize,
+            nodeLimit: nodeSearchPageSize,
+            fullTemplateInfo: true,
+            additionalNodes: bottom
+        });
+
+        const { availablePortTypes } = rootState.application;
+        const withMappedPorts = nodes.map(toNodeWithFullPorts(availablePortTypes));
+
+        commit(`setTotalNum${prefix}Nodes`, totalNumNodes);
+        commit(append ? `add${prefix}Nodes` : `set${prefix}Nodes`, withMappedPorts);
+        commit(`set${prefix}NodesTags`, tags);
+    },
+
+    /**
+     * Dispatches the search of nodes. If isShowingBottomNodes is true also a search for more nodes is dispatched.
+     * Otherwise, the results for more nodes are cleared.
+     *
+     * @param {*} context - Vuex context.
+     * @returns {Promise<void>}
+     */
+    async searchTopAndBottomNodes({ dispatch, state }) {
+        await Promise.all([
+            dispatch('searchNodes'),
+            state.isShowingBottomNodes
+                ? dispatch('searchNodes', { bottom: true })
+                : dispatch('clearSearchResultsForBottomNodes')
+        ]);
+    },
+
+    /**
+     * Clear search results (nodes and tags)
+     * @param {*} context - Vuex context.
+     * @returns {undefined}
+     */
+    async clearSearchResults({ commit, dispatch }) {
+        commit('setTopNodes', null);
+        commit('setTopNodesTags', []);
+        commit('setTotalNumTopNodes', 0);
+        await dispatch('clearSearchResultsForBottomNodes');
+    },
+
+    clearSearchResultsForBottomNodes({ commit }) {
+        commit('setBottomNodes', null);
+        commit('setBottomNodesTags', []);
+        commit('setTotalNumBottomNodes', 0);
+    },
+
+
+    /**
+     * Fetch the next page of node results.
+     *
+     * @param {*} context - Vuex context.
+     * @returns {undefined}
+     */
+    async searchTopNodesNextPage({ dispatch, state }) {
+        if (state.topNodes?.length !== state.totalNumTopNodes) {
+            await dispatch('searchNodes', { append: true });
+        }
+    },
+
+    /**
+     * Fetch the next page of node results for more nodes.
+     *
+     * @param {*} context - Vuex context.
+     * @returns {undefined}
+     */
+    async searchBottomNodesNextPage({ dispatch, state }) {
+        if (state.isShowingBottomNodes && state.bottomNodes?.length !== state.totalNumBottomNodes) {
+            await dispatch('searchNodes', { append: true, bottom: true });
+        }
+    },
+
+    /**
+     * Update all selected tags
+     *
+     * @param {*} context - Vuex context.
+     * @param {Array<String>} tags - array of tags to set
+     * @returns {undefined}
+     */
+    async setSelectedTags({ dispatch, commit }, tags) {
+        commit('setSelectedTags', tags);
+        await dispatch('searchTopAndBottomNodes');
+    },
+
+    async toggleShowingBottomNodes({ commit, dispatch, state }) {
+        commit('setShowingBottomNodes', !state.isShowingBottomNodes);
+        if (state.isShowingBottomNodes && state.bottomNodes === null) {
+            await dispatch('searchNodes', { bottom: true });
+        }
+    },
+
+    /**
+     * Update the value of the single search node query
+     *
+     * @param {*} context - Vuex context.
+     * @param {String} value - Search query value
+     * @returns {undefined}
+     */
+    async updateQuery({ commit, dispatch }, value) {
+        commit('setQuery', value);
+        await dispatch('searchTopAndBottomNodes');
+    },
+
+    /**
+     * Clear search parameter (query and selectedTags) and results
+     *
+     * @param {*} context - Vuex context.
+     * @returns {undefined}
+     */
+    async clearSearchParams({ commit, dispatch }) {
+        commit('setSelectedTags', []);
+        commit('setQuery', '');
+        await dispatch('clearSearchResults');
+    }
+};
+
+export const getters = {
+    hasSearchParams: state => state.query !== '' || state.selectedTags.length > 0,
+    searchIsActive: state => Boolean(state.query || state.topNodesTags.length) && state.topNodes !== null,
+    searchResultsContainSelectedNode(state) {
+        return Boolean(state.topNodes?.some(node => node.id === state.selectedNode?.id)) ||
+            (state.isShowingBottomNodes &&
+                Boolean(state.bottomNodes?.some(node => node.id === state.selectedNode?.id)));
+    },
+    tagsOfVisibleNodes: state => {
+        const allTags = [
+            ...state.topNodesTags,
+            ...state.selectedTags,
+            // eslint-disable-next-line no-extra-parens
+            ...(state.isShowingBottomNodes ? state.bottomNodesTags : [])
+        ];
+        return [...new Set(allTags)];
+    }
+};
--- org.knime.ui.js/src/store/nodeRepository.js
+++ org.knime.ui.js/src/store/nodeRepository.js
@@ -1,34 +1,23 @@
-import { searchNodes, getNodesGroupedByTags, getNodeDescription, getNodeTemplates } from '@api';
+import { getNodesGroupedByTags, getNodeDescription, getNodeTemplates } from '@api';
 import { toNodeWithFullPorts } from '../util/portDataMapper';
+import * as nodeSearch from './common/nodeSearch';
 
 /**
  * Store that manages node repository state.
  */
 
-const nodeSearchPageSize = 100;
+
 const categoryPageSize = 3;
 const firstLoadOffset = 6;
 
 export const state = () => ({
+    ...nodeSearch.state,
+
     /* categories */
     nodesPerCategory: [],
     totalNumCategories: null,
     categoryPage: 0,
     categoryScrollPosition: 0,
-
-    /* search results */
-    topNodes: null,
-    totalNumTopNodes: 0,
-    selectedTags: [],
-    topNodesTags: [],
-    query: '',
-    topNodeSearchPage: 0,
-    searchScrollPosition: 0,
-    isShowingBottomNodes: false,
-    bottomNodes: null,
-    totalNumBottomNodes: 0,
-    bottomNodesTags: [],
-    bottomNodeSearchPage: 0,
 
     /* node interaction */
     selectedNode: null,
@@ -40,91 +29,42 @@ export const state = () => ({
 });
 
 export const mutations = {
+    ...nodeSearch.mutations,
 
     setCategoryPage(state, pageNumber) {
         state.categoryPage = pageNumber;
     },
-    setTopNodeSearchPage(state, pageNumber) {
-        state.topNodeSearchPage = pageNumber;
-    },
 
-    setTotalNumTopNodes(state, totalNumTopNodes) {
-        state.totalNumTopNodes = totalNumTopNodes;
-    },
-
-    addTopNodes(state, topNodes) {
-        let existingNodeIds = state.topNodes.map(node => node.id);
-        let newNodes = topNodes.filter(node => !existingNodeIds.includes(node.id));
-        state.topNodes.push(...newNodes);
-    },
-
-    setTopNodes(state, topNodes) {
-        state.topNodes = topNodes;
-    },
-
-    setTopNodesTags(state, topNodesTags) {
-        state.topNodesTags = topNodesTags;
-    },
-
-    setBottomNodeSearchPage(state, pageNumber) {
-        state.bottomNodeSearchPage = pageNumber;
-    },
-
-    setTotalNumBottomNodes(state, totalNumBottomNodes) {
-        state.totalNumBottomNodes = totalNumBottomNodes;
-    },
-
-    addBottomNodes(state, bottomNodes) {
-        let existingNodeIds = state.bottomNodes.map(node => node.id);
-        let newNodes = bottomNodes.filter(node => !existingNodeIds.includes(node.id));
-        state.bottomNodes.push(...newNodes);
-    },
-
-    setBottomNodes(state, bottomNodes) {
-        state.bottomNodes = bottomNodes;
-    },
-
-    setBottomNodesTags(state, bottomNodesTags) {
-        state.bottomNodesTags = bottomNodesTags;
-    },
-
-    setSelectedTags(state, selectedTags) {
-        state.selectedTags = selectedTags;
-        state.searchScrollPosition = 0;
-    },
     setNodesPerCategories(state, { groupedNodes, append }) {
         state.nodesPerCategory = append
             ? state.nodesPerCategory.concat(groupedNodes)
             : groupedNodes;
     },
-    setQuery(state, value) {
-        state.query = value;
-        state.searchScrollPosition = 0;
-    },
+
     setTotalNumCategories(state, totalNumCategories) {
         state.totalNumCategories = totalNumCategories;
     },
-    setSearchScrollPosition(state, value) {
-        state.searchScrollPosition = value;
-    },
+
     setCategoryScrollPosition(state, value) {
         state.categoryScrollPosition = value;
     },
+
     setSelectedNode(state, node) {
         state.selectedNode = node;
     },
+
     setDraggingNode(state, value) {
         state.isDraggingNode = value;
     },
+
     setDescriptionPanel(state, value) {
         state.isDescriptionPanelOpen = value;
-    },
-    setShowingBottomNodes(state, value) {
-        state.isShowingBottomNodes = value;
     }
 };
 
 export const actions = {
+    ...nodeSearch.actions,
+
     async getAllNodes({ commit, state, rootState }, { append }) {
         if (state.nodesPerCategory.length === state.totalNumCategories) {
             return;
@@ -156,113 +96,12 @@ export const actions = {
         commit('setNodesPerCategories', { groupedNodes: withMappedPorts, append });
     },
 
-    /**
-     * Fetch nodes. Used for initial data retrieval, but also for searching via query and/or tag filters.
-     *
-     * @param {*} context - Vuex context.
-     * @param {Boolean} append - if the results should be added to the current nodes (for pagination) or if the state
-     *      should be cleared (for a new search).
-     * @param {Boolean} bottom - if search should be for additional nodes that are displayed on the bottom
-     * @returns {Promise}
-     */
-    async searchNodes({ commit, state, dispatch, getters, rootState }, { append = false, bottom = false } = {}) {
-        // only do request if we search for something
-        if (!getters.hasSearchParams) {
-            // clear old results
-            dispatch('clearSearchResults');
-            return;
-        }
-        const prefix = bottom ? 'Bottom' : 'Top';
-        const currentPage = () => bottom ? state.bottomNodeSearchPage : state.topNodeSearchPage;
-
-        if (append) {
-            commit(`set${prefix}NodeSearchPage`, currentPage() + 1);
-        } else {
-            commit(`set${prefix}NodeSearchPage`, 0);
-        }
-
-        const { nodes, totalNumNodes, tags } = await searchNodes({
-            query: state.query,
-            tags: state.selectedTags,
-            allTagsMatch: true,
-            nodeOffset: currentPage() * nodeSearchPageSize,
-            nodeLimit: nodeSearchPageSize,
-            fullTemplateInfo: true,
-            additionalNodes: bottom
-        });
-
-        const { availablePortTypes } = rootState.application;
-        const withMappedPorts = nodes.map(toNodeWithFullPorts(availablePortTypes));
-
-        commit(`setTotalNum${prefix}Nodes`, totalNumNodes);
-        commit(append ? `add${prefix}Nodes` : `set${prefix}Nodes`, withMappedPorts);
-        commit(`set${prefix}NodesTags`, tags);
-    },
-
-    /**
-     * Dispatches the search of nodes. If isShowingBottomNodes is true also a search for more nodes is dispatched.
-     * Otherwise, the results for more nodes are cleared.
-     *
-     * @param {*} context - Vuex context.
-     * @returns {Promise<void>}
-     */
-    async searchTopAndBottomNodes({ dispatch, state }) {
-        await Promise.all([
-            dispatch('searchNodes'),
-            state.isShowingBottomNodes
-                ? dispatch('searchNodes', { bottom: true })
-                : dispatch('clearSearchResultsForBottomNodes')
-        ]);
-    },
-
     async getNodeDescription({ commit, state, rootState }, { selectedNode }) {
         const { className, settings } = selectedNode.nodeFactory;
         const node = await getNodeDescription({ className, settings });
 
         const { availablePortTypes } = rootState.application;
         return toNodeWithFullPorts(availablePortTypes)(node);
-    },
-
-    /**
-     * Update the value of the single search node query
-     *
-     * @param {*} context - Vuex context.
-     * @param {String} value - Search query value
-     * @returns {undefined}
-     */
-    async updateQuery({ commit, dispatch }, value) {
-        commit('setQuery', value);
-        await dispatch('searchTopAndBottomNodes');
-    },
-
-    /**
-     * Clear search parameter (query and selectedTags) and results
-     *
-     * @param {*} context - Vuex context.
-     * @returns {undefined}
-     */
-    async clearSearchParams({ commit, dispatch }) {
-        commit('setSelectedTags', []);
-        commit('setQuery', '');
-        await dispatch('clearSearchResults');
-    },
-
-    /**
-     * Clear search results (nodes and tags)
-     * @param {*} context - Vuex context.
-     * @returns {undefined}
-     */
-    async clearSearchResults({ commit, dispatch }) {
-        commit('setTopNodes', null);
-        commit('setTopNodesTags', []);
-        commit('setTotalNumTopNodes', 0);
-        await dispatch('clearSearchResultsForBottomNodes');
-    },
-
-    clearSearchResultsForBottomNodes({ commit }) {
-        commit('setBottomNodes', null);
-        commit('setBottomNodesTags', []);
-        commit('setTotalNumBottomNodes', 0);
     },
 
     clearCategoryResults({ commit }) {
@@ -272,55 +111,12 @@ export const actions = {
         commit('setCategoryScrollPosition', 0);
     },
 
-    /**
-     * Fetch the next page of node results.
-     *
-     * @param {*} context - Vuex context.
-     * @returns {undefined}
-     */
-    async searchTopNodesNextPage({ dispatch, state }) {
-        if (state.topNodes?.length !== state.totalNumTopNodes) {
-            await dispatch('searchNodes', { append: true });
-        }
-    },
-
-    /**
-     * Fetch the next page of node results for more nodes.
-     *
-     * @param {*} context - Vuex context.
-     * @returns {undefined}
-     */
-    async searchBottomNodesNextPage({ dispatch, state }) {
-        if (state.isShowingBottomNodes && state.bottomNodes?.length !== state.totalNumBottomNodes) {
-            await dispatch('searchNodes', { append: true, bottom: true });
-        }
-    },
-
-    /**
-     * Update all selected tags
-     *
-     * @param {*} context - Vuex context.
-     * @param {Array<String>} tags - array of tags to set
-     * @returns {undefined}
-     */
-    async setSelectedTags({ dispatch, commit }, tags) {
-        commit('setSelectedTags', tags);
-        await dispatch('searchTopAndBottomNodes');
-    },
-
     openDescriptionPanel({ commit }) {
         commit('setDescriptionPanel', true);
     },
 
     closeDescriptionPanel({ commit }) {
         commit('setDescriptionPanel', false);
-    },
-
-    async toggleShowingBottomNodes({ commit, dispatch, state }) {
-        commit('setShowingBottomNodes', !state.isShowingBottomNodes);
-        if (state.isShowingBottomNodes && state.bottomNodes === null) {
-            await dispatch('searchNodes', { bottom: true });
-        }
     },
 
     async resetSearchAndCategories({ dispatch, getters }) {
@@ -348,13 +144,8 @@ export const actions = {
 };
 
 export const getters = {
-    hasSearchParams: state => state.query !== '' || state.selectedTags.length > 0,
-    searchIsActive: state => Boolean(state.query || state.topNodesTags.length) && state.topNodes !== null,
-    searchResultsContainSelectedNode(state) {
-        return Boolean(state.topNodes?.some(node => node.id === state.selectedNode?.id)) ||
-            (state.isShowingBottomNodes &&
-                Boolean(state.bottomNodes?.some(node => node.id === state.selectedNode?.id)));
-    },
+    ...nodeSearch.getters,
+
     nodesPerCategoryContainSelectedNode(state) {
         return state.nodesPerCategory.some(category => category.nodes.some(
             node => node.id === state.selectedNode?.id
@@ -362,14 +153,5 @@ export const getters = {
     },
     isSelectedNodeVisible: (state, getters) => getters.searchIsActive
         ? getters.searchResultsContainSelectedNode
-        : getters.nodesPerCategoryContainSelectedNode,
-    tagsOfVisibleNodes: state => {
-        const allTags = [
-            ...state.topNodesTags,
-            ...state.selectedTags,
-            // eslint-disable-next-line no-extra-parens
-            ...(state.isShowingBottomNodes ? state.bottomNodesTags : [])
-        ];
-        return [...new Set(allTags)];
-    }
+        : getters.nodesPerCategoryContainSelectedNode
 };
