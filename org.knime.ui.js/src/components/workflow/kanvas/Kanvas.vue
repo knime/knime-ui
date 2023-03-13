8ac951a01a44058a1cdb2c4163f66bfd31da4535
Gosia Wojcik <gosia.wojcik@knime.com>
2023-03-13 17:30:41 +0100

    NXT-1449: Fix linting error
    NXT-1449 (Enable also multiple node selection with Ctrl / ⌘ and rectangle selection)

--- org.knime.ui.js/src/components/workflow/kanvas/Kanvas.vue
+++ org.knime.ui.js/src/components/workflow/kanvas/Kanvas.vue
@@ -125,7 +125,7 @@ export default {
             // we still want to zoom on ctrl or meta key.
             // Note: The pinch-to-zoom gesture on Mac causes a wheel event with ctrlKey=True,
             //       so we need to check for it to obtain zoom on pinch-to-zoom.
-            const shouldZoom = this.scrollToZoomEnabled || event.ctrlKey || (isMac() && event.metaKey);;
+            const shouldZoom = this.scrollToZoomEnabled || event.ctrlKey || (isMac() && event.metaKey);
             if (!shouldZoom) {
                 return;
             }
