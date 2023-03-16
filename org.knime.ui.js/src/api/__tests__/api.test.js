adee55446902fa4fd2a1fa7d9ecd190f71cc8499
Helian Rivera <helian.rivera@knime.com>
2023-03-16 16:03:33 +0100

    NXT-1627: remove unneeded tests
    NXT-1627 (Migrate Gateway API calls in "knime-ui" to use the automatically generated TS implementations)

--- org.knime.ui.js/src/api/__tests__/api.test.js
+++ org.knime.ui.js/src/api/__tests__/api.test.js
@@ -2,7 +2,6 @@ import { expect, describe, beforeAll, beforeEach, afterAll, it, vi } from 'vites
 /* eslint-disable max-lines */
 /* eslint-disable consistent-return */
 import * as api from '@api';
-import { waitForPatch } from '@/util/event-syncer';
 
 const origErrorLogger = window.consola.error;
 
@@ -161,7 +160,7 @@ describe('API', () => {
                     id: 0
                 });
             });
-                
+
             it('calls jsonrpc without portTypeId', async () => {
                 const params = {
                     tags: ['myTag'],
@@ -176,7 +175,7 @@ describe('API', () => {
                     query,
                     ...params
                 });
-    
+
                 expect(window.jsonrpc).toHaveBeenCalledWith({
                     jsonrpc: '2.0',
                     method: 'NodeRepositoryService.searchNodes',
@@ -417,68 +416,6 @@ describe('API', () => {
     });
 
     describe('workflow Service', () => {
-        it('does not wait for patch if command does not have snapshot id', async () => {
-            window.jsonrpc.mockReturnValueOnce({
-                jsonrpc: '2.0',
-                result: {},
-                id: -1
-            });
-
-            await api.collapseToContainer({ projectId: '123', workflowId: '12' });
-
-            expect(waitForPatch).not.toHaveBeenCalled();
-            expect(window.jsonrpc).toHaveBeenCalled();
-        });
-
-        // it('commands with snapshot id wait for patches with corresponding snapshot id to resolve', async () => {
-        //     let isFirstPatchResolved = false;
-        //     let isSecondPatchResolved = false;
-        //     const firstSnapshotId = 1;
-        //     const secondSnapshotId = 2;
-
-        //     waitForPatch.mockImplementation(
-        //         // eslint-disable-next-line max-nested-callbacks
-        //         (params) => new Promise(resolve => setTimeout(() => {
-        //             if (params === firstSnapshotId) {
-        //                 isFirstPatchResolved = true;
-        //             }
-
-        //             if (params === secondSnapshotId) {
-        //                 isSecondPatchResolved = true;
-        //             }
-        //             resolve('patch complete');
-        //         }, 100))
-        //     );
-
-        //     window.jsonrpc
-        //         .mockReturnValueOnce({
-        //             jsonrpc: '2.0',
-        //             result: { snapshotId: firstSnapshotId },
-        //             id: -1
-        //         })
-        //         .mockReturnValueOnce({
-        //             jsonrpc: '2.0',
-        //             result: { snapshotId: secondSnapshotId },
-        //             id: -1
-        //         });
-
-        //     const firstCommandCall = api.collapseToContainer({ projectId: '123', workflowId: '12' });
-        //     const secondCommandCall = api.collapseToContainer({ projectId: '123', workflowId: '12' });
-
-        //     expect(isFirstPatchResolved).toBe(false);
-        //     expect(isSecondPatchResolved).toBe(false);
-
-        //     await firstCommandCall;
-
-        //     expect(isFirstPatchResolved).toBe(true);
-        //     expect(isSecondPatchResolved).toBe(false);
-
-        //     await secondCommandCall;
-
-        //     expect(isSecondPatchResolved).toBe(true);
-        //     expect(window.jsonrpc).toHaveBeenCalledTimes(2);
-        // });
-
         it('deletes objects (empty)', async () => {
             const projectId = '123';
             const workflowId = '12';
