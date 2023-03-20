f784e61e2d8b5ada2099983c84a2f49ff99212b1
Helian Rivera <helian.rivera@knime.com>
2023-03-20 12:20:43 +0100

    remove service wrappers

--- org.knime.ui.js/src/api/index.ts
+++ org.knime.ui.js/src/api/index.ts
@@ -5,11 +5,3 @@ export const API = {
     ...gateway,
     desktop
 };
-
-export * from './applicationService';
-export * from './nodeRepositoryService';
-export * from './nodeService';
-export * from './portService';
-export * from './workflowService';
-export * from './desktopApi';
-export * from './spaceService';
