e941ab93d3033116b4cc06f3d63d1b7aeb0b0a8d
Juan Jose Diaz Baquero <juan.diazbaquero@tngtech.com>
2023-05-16 13:20:50 +0200

    NXT-1718: add tests to paste strategies
    NXT-1718 (Enable Cut and Copy in context menu with only workflow annotations selected)

--- org.knime.ui.js/src/util/__tests__/pasteToWorkflow.test.ts
+++ org.knime.ui.js/src/util/__tests__/pasteToWorkflow.test.ts
@@ -1,22 +1,32 @@
-import { describe, it, expect } from 'vitest';
-import { offsetStrategy } from '@/util/pasteToWorkflow';
+import { describe, it, expect, vi } from 'vitest';
+import { offsetStrategy, centerStrategy, pastePartsAt } from '@/util/pasteToWorkflow';
 
 describe('pasteToWorkflow', () => {
+    const randomSpy = vi.spyOn(Math, 'random').mockImplementation(() => 0);
+    const mockClipboardContent = { objectBounds: {
+        left: 0,
+        top: 0,
+        width: 10,
+        height: 10
+    } };
+
+    const mockVisibleFrame = {
+        left: 0,
+        top: 0,
+        width: 1000,
+        height: 1000
+    };
+
+    describe('center strategy', () => {
+        it('returns a position with offset', () => {
+            const position = centerStrategy({ clipboardContent: mockClipboardContent, visibleFrame: mockVisibleFrame });
+            expect(randomSpy).toBeCalledTimes(2);
+            expect(position.x).toBe(470);
+            expect(position.y).toBe(345);
+        });
+    });
+
     describe('offSet strategy', () => {
-        const mockClipboardContent = { objectBounds: {
-            left: 0,
-            top: 0,
-            width: 10,
-            height: 10
-        } };
-
-        const mockVisibleFrame = {
-            left: 0,
-            top: 0,
-            width: 1000,
-            height: 1000
-        };
-
         it('returns a position with offset', () => {
             const position = offsetStrategy({ clipboardContent: mockClipboardContent, visibleFrame: mockVisibleFrame });
             expect(position.x).toBeGreaterThan(0);
@@ -29,6 +39,38 @@ describe('pasteToWorkflow', () => {
                 visibleFrame: { ...mockVisibleFrame, width: 5, height: 5 }
             });
             expect(position).toBeNull();
+        });
+    });
+
+    describe('pastePartsAt', () => {
+        it('uses center strategy when workflow is empty', () => {
+            const dispatch = vi.fn();
+            const paste = pastePartsAt({ visibleFrame: mockVisibleFrame,
+                clipboardContent: mockClipboardContent,
+                isWorkflowEmpty: true,
+                dispatch });
+            expect(paste.position.x).toBe(470);
+            expect(paste.position.y).toBe(345);
+        });
+
+        it('uses offset strategy first', () => {
+            const dispatch = vi.fn();
+            const paste = pastePartsAt({ visibleFrame: mockVisibleFrame,
+                clipboardContent: mockClipboardContent,
+                isWorkflowEmpty: false,
+                dispatch });
+            expect(paste.position.x).toBe(95);
+            expect(paste.position.y).toBe(95);
+        });
+
+        it('uses center strategy when paste is not visible', () => {
+            const dispatch = vi.fn();
+            const paste = pastePartsAt({ visibleFrame: mockVisibleFrame,
+                clipboardContent: { objectBounds: { ...mockClipboardContent.objectBounds, left: 3000, top: 3000 } },
+                isWorkflowEmpty: false,
+                dispatch });
+            expect(paste.position.x).toBe(470);
+            expect(paste.position.y).toBe(345);
         });
     });
 });
