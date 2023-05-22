97b0ae16acf80623b9433b30e25f3c0cc123a470
Helian Rivera <helian.rivera@knime.com>
2023-05-22 08:39:10 +0200

    NXT-1737: refactor directive move to TS and fix bug with pointer capture
    NXT-1737 (Frontend selection/moving events are broken after moving nodes and annotations around)

---
+++ org.knime.ui.js/src/plugins/__tests__/directive-move.test.ts
@@ -0,0 +1,219 @@
+import { expect, describe, beforeAll, it, vi, afterEach } from "vitest";
+import * as Vue from "vue";
+import { mount, config, VueWrapper } from "@vue/test-utils";
+
+import { directiveMove } from "../directive-move";
+
+describe("directive-move", () => {
+  beforeAll(() => {
+    config.global.directives = {
+      [directiveMove.name]: directiveMove.options,
+    };
+
+    HTMLElement.prototype.hasPointerCapture = vi.fn();
+    HTMLElement.prototype.setPointerCapture = vi.fn();
+    HTMLElement.prototype.releasePointerCapture = vi.fn();
+  });
+
+  afterEach(() => {
+    vi.clearAllMocks();
+  });
+
+  const doMount = ({
+    onMove = vi.fn(),
+    onMoveStart = vi.fn(),
+    onMoveEnd = vi.fn(),
+  } = {}) => {
+    const template = `
+    <div v-move="{
+      onMove,
+      onMoveStart,
+      onMoveEnd
+    }"></div>
+    `;
+
+    const wrapper = mount({
+      methods: { onMove, onMoveStart, onMoveEnd },
+      template,
+    });
+
+    const hasPointerCapture = vi.fn();
+    const setPointerCapture = vi.fn();
+    const releasePointerCapture = vi.fn();
+    wrapper.element.hasPointerCapture = hasPointerCapture;
+    wrapper.element.setPointerCapture = setPointerCapture;
+    wrapper.element.releasePointerCapture = releasePointerCapture;
+
+    return {
+      wrapper,
+      onMove,
+      onMoveStart,
+      onMoveEnd,
+      hasPointerCapture,
+      setPointerCapture,
+      releasePointerCapture,
+    };
+  };
+
+  const dispatchPointerEvent = (
+    wrapper: VueWrapper<any>,
+    eventType:
+      | "pointerdown"
+      | "pointermove"
+      | "pointerup"
+      | "lostpointercapture",
+    { clientX = 0, clientY = 0, button = 0 }
+  ) => {
+    const pointerEvent = new Event(eventType);
+
+    // @ts-ignore
+    pointerEvent.clientX = clientX;
+    // @ts-ignore
+    pointerEvent.clientY = clientY;
+    // @ts-ignore
+    pointerEvent.button = button;
+    pointerEvent.stopPropagation = vi.fn();
+    pointerEvent.preventDefault = vi.fn();
+
+    wrapper.element.dispatchEvent(pointerEvent);
+
+    return pointerEvent;
+  };
+
+  it("should not try to move without first clicking", async () => {
+    const { wrapper, onMove } = doMount();
+
+    dispatchPointerEvent(wrapper, "pointermove", {
+      clientX: 100,
+      clientY: 100,
+    });
+
+    await Vue.nextTick();
+    expect(onMove).not.toHaveBeenCalled();
+  });
+
+  it("should handle moving elements", async () => {
+    const { wrapper, setPointerCapture, onMoveStart, onMove } = doMount();
+
+    dispatchPointerEvent(wrapper, "pointerdown", {
+      clientX: 50,
+      clientY: 50,
+    });
+
+    const moveEvent = dispatchPointerEvent(wrapper, "pointermove", {
+      clientX: 100,
+      clientY: 100,
+    });
+
+    await Vue.nextTick();
+
+    expect(onMoveStart).toHaveBeenCalledWith(
+      expect.objectContaining({
+        detail: expect.objectContaining({
+          startX: 50,
+          startY: 50,
+          // move start gets passed the move event, not the start event. This is by design
+          event: moveEvent,
+        }),
+      })
+    );
+
+    expect(setPointerCapture).toHaveBeenCalled();
+
+    expect(onMove).toHaveBeenCalledWith(
+      expect.objectContaining({
+        detail: expect.objectContaining({
+          deltaX: 50,
+          deltaY: 50,
+          event: moveEvent,
+          clientX: 100,
+          clientY: 100,
+        }),
+      })
+    );
+
+    dispatchPointerEvent(wrapper, "pointermove", {
+      clientX: 150,
+      clientY: 150,
+    });
+
+    expect(onMove).toHaveBeenLastCalledWith(
+      expect.objectContaining({
+        detail: expect.objectContaining({
+          deltaX: 50,
+          deltaY: 50,
+          clientX: 150,
+          clientY: 150,
+        }),
+      })
+    );
+  });
+
+  it("should only work with the left button", () => {
+    const { wrapper, onMoveStart } = doMount();
+
+    dispatchPointerEvent(wrapper, "pointerdown", {
+      clientX: 50,
+      clientY: 50,
+      button: 1,
+    });
+
+    expect(onMoveStart).not.toHaveBeenCalled();
+  });
+
+  it("shoud handle move end", () => {
+    const { wrapper, onMoveEnd, releasePointerCapture } = doMount();
+
+    dispatchPointerEvent(wrapper, "pointerdown", {
+      clientX: 50,
+      clientY: 50,
+    });
+
+    dispatchPointerEvent(wrapper, "pointermove", {
+      clientX: 100,
+      clientY: 100,
+    });
+
+    dispatchPointerEvent(wrapper, "pointerup", {
+      clientX: 100,
+      clientY: 100,
+    });
+
+    expect(onMoveEnd).toHaveBeenLastCalledWith(
+      expect.objectContaining({
+        detail: expect.objectContaining({
+          startX: 50,
+          startY: 50,
+          endX: 100,
+          endY: 100,
+        }),
+      })
+    );
+
+    expect(releasePointerCapture).toHaveBeenCalled();
+  });
+
+  it("removes the listener", () => {
+    const { wrapper, onMoveStart, onMove, onMoveEnd } = doMount();
+    wrapper.unmount();
+
+    dispatchPointerEvent(wrapper, "pointerdown", {
+      clientX: 50,
+      clientY: 50,
+    });
+
+    dispatchPointerEvent(wrapper, "pointermove", {
+      clientX: 100,
+      clientY: 100,
+    });
+
+    dispatchPointerEvent(wrapper, "pointerup", {
+      clientX: 100,
+      clientY: 100,
+    });
+
+    expect(onMoveStart).not.toHaveBeenCalled();
+    expect(onMove).not.toHaveBeenCalled();
+    expect(onMoveEnd).not.toHaveBeenCalled();
+  });
+});
---
+++ org.knime.ui.js/src/plugins/directive-move.ts
@@ -0,0 +1,260 @@
+/**
+ * Drag & Drop Vue directive.
+ *
+ * @example:
+ * <el v-move="{ onMove, onMoveStart, onMoveEnd, threshold: 5 }" />
+ *
+ * @param {Function=} onMoveStart Optional handler for the `movestart` event
+ * @param {Function=} onMove Optional handler for the `moving` event
+ * @param {Function=} onMoveEnd Optional handler for the `moveend` event
+ * @param {Number=} threshold Distance that the mouse must have travelled before a move event is fired. Defaults to 5.
+ * @param {boolean=} isProtected Only applies the directive if is false
+ *
+ * The movestart, moving, and moveend events have a `detail` property which holds the attributes
+ * `startX`, `startY`, `deltaX`, `deltaY`, `totalDeltaX`, `totalDeltaY`, `endX`, `endY` and `e`
+ * respectively, depending on the event type.
+ */
+
+import type { Directive } from "vue";
+
+type MoveStartEventDetail = {
+  startX: number;
+  startY: number;
+  clientX: number;
+  clientY: number;
+  event: PointerEvent;
+};
+
+type MoveEventDetail = {
+  deltaX: number;
+  deltaY: number;
+  clientX: number;
+  clientY: number;
+  altKey: boolean;
+  event: PointerEvent;
+};
+
+type MoveEndEventDetail = {
+  startX: number;
+  startY: number;
+  endX: number;
+  endY: number;
+  event: PointerEvent;
+};
+
+type DirectiveBinding = {
+  isProtected: boolean;
+  threshold?: number;
+
+  onMoveStart?: (event: CustomEvent<MoveStartEventDetail>) => any;
+  onMove?: (event: CustomEvent<MoveEventDetail>) => any;
+  onMoveEnd?: (event: CustomEvent<MoveEndEventDetail>) => any;
+};
+
+interface MoveState {
+  /**
+   * Whether the element that this state corresponds to is being dragged
+   */
+  dragging: boolean;
+  /**
+   * Callbacks to call when each of the drag lifecycle events happen
+   */
+  handlers: Pick<DirectiveBinding, "onMoveStart" | "onMove" | "onMoveEnd">;
+
+  /**
+   * Stored reference of the registered pointerdown event handler for the native
+   * pointer event
+   * @param event native event
+   */
+  pointerdownHandler: (event: PointerEvent) => void;
+  /**
+   * Stored reference of the registered pointermove event handler for the native
+   * pointer event
+   * @param event native event
+   */
+  pointermoveHandler: (event: PointerEvent) => void;
+  /**
+   * Stored reference of the registered pointerup event handler for the native
+   * pointer event
+   * @param event native event
+   */
+  pointerupHandler: (event: PointerEvent) => void;
+
+  /** Pointer ID that has the pointercapture */
+  pointerId?: number;
+  /** Position where the drag started */
+  start?: [number, number];
+  /** Store the position of the previous move (if any) */
+  prev?: [number, number];
+}
+
+const stateMap: WeakMap<HTMLElement, MoveState> = new WeakMap();
+
+const createPointerdownHandler =
+  (srcElement: HTMLElement) => (event: PointerEvent) => {
+    const state = stateMap.get(srcElement);
+
+    // only left mouse button can move
+    if (event.button !== 0) {
+      return;
+    }
+
+    event.stopPropagation();
+    event.preventDefault();
+
+    const { pointerId } = event;
+    state.pointerId = pointerId;
+
+    srcElement.addEventListener("pointermove", state.pointermoveHandler);
+
+    const { clientX, clientY } = event;
+    delete state.prev;
+    state.start = [clientX, clientY];
+  };
+
+const createPointermoveHandler =
+  (srcElement: HTMLElement) => (event: PointerEvent) => {
+    const state = stateMap.get(srcElement);
+
+    if (!state.start) {
+      return;
+    }
+
+    const [startX, startY] = state.start;
+    const { clientX, clientY } = event;
+    const [moveX, moveY] = [clientX - startX, clientY - startY];
+
+    if (!srcElement.hasPointerCapture(state.pointerId)) {
+      srcElement.setPointerCapture(state.pointerId);
+    }
+
+    event.stopPropagation();
+    event.preventDefault();
+
+    // dispatch `movestart` event for the first move
+    if (!state.dragging && state.handlers.onMoveStart) {
+      const moveStartEvent = new CustomEvent<MoveStartEventDetail>(
+        "movestart",
+        {
+          detail: {
+            startX,
+            startY,
+            event,
+            clientX: event.clientX,
+            clientY: event.clientY,
+          },
+        }
+      );
+      state.handlers.onMoveStart(moveStartEvent);
+    }
+
+    state.dragging = true;
+
+    const deltaX = state.prev ? moveX - state.prev[0] : moveX;
+    const deltaY = state.prev ? moveY - state.prev[1] : moveY;
+
+    state.prev = [moveX, moveY];
+
+    if (state.handlers.onMove) {
+      const moveEvent = new CustomEvent<MoveEventDetail>("moving", {
+        detail: {
+          deltaX,
+          deltaY,
+          event,
+          altKey: event.altKey,
+          clientX: event.clientX,
+          clientY: event.clientY,
+        },
+      });
+
+      state.handlers.onMove(moveEvent);
+    }
+  };
+
+const createPointerupHandler =
+  (srcElement: HTMLElement) => (event: PointerEvent) => {
+    const state = stateMap.get(srcElement);
+
+    srcElement.releasePointerCapture(event.pointerId);
+    srcElement.removeEventListener("pointermove", state.pointermoveHandler);
+    delete state.pointerId;
+
+    if (state.dragging && state.handlers.onMoveEnd) {
+      const { clientX, clientY } = event;
+      const [startX, startY] = state.start;
+      const moveEndEvent = new CustomEvent<MoveEndEventDetail>("moveend", {
+        detail: {
+          startX,
+          startY,
+          endX: clientX,
+          endY: clientY,
+          event,
+        },
+      });
+
+      state.handlers.onMoveEnd(moveEndEvent);
+    }
+
+    delete state.start;
+    delete state.dragging;
+  };
+
+const options: Directive<HTMLElement, DirectiveBinding> = {
+  mounted: (el, { value }) => {
+    // Only insert when the object is writable
+    if (value.isProtected) {
+      return;
+    }
+
+    const state: MoveState = {
+      handlers: value,
+      dragging: false,
+      pointerdownHandler: createPointerdownHandler(el),
+      pointermoveHandler: createPointermoveHandler(el),
+      pointerupHandler: createPointerupHandler(el),
+    };
+
+    el.addEventListener("pointerdown", state.pointerdownHandler);
+    el.addEventListener("pointerup", state.pointerupHandler);
+    el.addEventListener("lostpointercapture", state.pointerupHandler);
+
+    stateMap.set(el, state);
+  },
+
+  unmounted: (el, { value }) => {
+    // Only insert when the object is writable
+    if (value.isProtected) {
+      return;
+    }
+    const state = stateMap.get(el);
+
+    el.removeEventListener("pointerdown", state.pointerdownHandler);
+    el.removeEventListener("pointermove", state.pointermoveHandler);
+    el.removeEventListener("pointerup", state.pointerupHandler);
+    el.removeEventListener("lostpointercapture", state.pointerupHandler);
+
+    stateMap.delete(el);
+  },
+
+  // reapply the pointer capture when the component changes
+  // this is necessary, as otherwise the capture is lost on rerender of the view
+  updated: (el, { value }) => {
+    // Only insert when the object is writable
+    if (value.isProtected) {
+      return;
+    }
+    const state = stateMap.get(el);
+
+    if (
+      Reflect.has(state, "pointerId") &&
+      !el.hasPointerCapture(state.pointerId)
+    ) {
+      el.setPointerCapture(state.pointerId);
+    }
+  },
+};
+
+export const directiveMove = {
+  name: "move",
+  options,
+};
