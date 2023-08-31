c524f75c460276c3ffce012cd280bae381b22cb2
Gosia Wojcik <gosia.wojcik@knime.com>
2023-08-31 14:28:44 +0200

    NXT-1973: Change test
    NXT-1973 (Display jobs FE)

--- org.knime.ui.js/src/util/__tests__/time.test.ts
+++ org.knime.ui.js/src/util/__tests__/time.test.ts
@@ -3,10 +3,10 @@ import { formatTime, addLeadingZero } from "@/util/time";
 
 describe("time", () => {
   it("correctly formats time from milliseconds", () => {
-    const date = new Date("Nov 21, 1987, 16:00").getTime();
+    const date = new Date("Nov 22, 1987, 16:00").getTime();
     const dateMilliseconds = date / 1000;
 
-    expect(formatTime(dateMilliseconds)).toContain("Nov 21, 1987, 16:00");
+    expect(formatTime(dateMilliseconds)).toContain("Nov 22, 1987, 16:00");
   });
 
   it("adds 0 if number of is smaller than 10", () => {
