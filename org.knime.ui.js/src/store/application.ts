b88c78990c372df1346be8931b0b3346846143d0
Martin Horn <martin.horn@knime.com>
2023-09-14 11:07:14 +0200

    NXT-2003: Catch error when registering for UpdateAvailableEvent
    Because on perspective switch there is no 'Update Provider' since we
    don't want to check for an update in that case.
    
    NXT-2003 (Release update banner wrong in case a direct upgrade is not possible)

--- org.knime.ui.js/src/store/application.ts
+++ org.knime.ui.js/src/store/application.ts
@@ -165,8 +165,12 @@ export const actions = {
    */
   async initializeApplication({ dispatch }, { $router }) {
     await API.event.subscribeEvent({ typeId: "AppStateChangedEventType" });
-    await API.event.subscribeEvent({ typeId: "UpdateAvailableEventType" });
-    API.desktop.checkForUpdates();
+    try {
+      await API.event.subscribeEvent({ typeId: "UpdateAvailableEventType" });
+      API.desktop.checkForUpdates();
+    } catch (error) {
+      consola.log(error);
+    }
 
     $router.beforeEach(async (to, from, next) => {
       const isLeavingWorkflow = from.name === APP_ROUTES.WorkflowPage;
