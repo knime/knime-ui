19e97f2c21b5d46fc6ac3bfba62f384f39c8aa29
Manuel Hotz <manuel.hotz@knime.com>
2023-09-20 14:53:44 +0200

    NXT-2008: Add deleteSchedulesForWorkflow gateway API
    NXT-2008 (Kebap menu items for schedules)

--- org.knime.ui.js/src/api/gateway-api/generated-api.ts
+++ org.knime.ui.js/src/api/gateway-api/generated-api.ts
@@ -4248,6 +4248,23 @@ const space = function(rpcClient: RPCClient) {
            return rpcClient.call('SpaceService.deleteJobsForWorkflow', { ...defaultParams, ...params });
         },
         /**
+         * Deletes schedule from the space.
+         * @param {string} spaceId The unique identifier of the space (local workspace, hub space). If &#39;local&#39; it refers to the local workspace.
+         * @param {string} spaceProviderId Identifies a space-provider.
+         * @param {string} itemId The unique identifier of the space item. If &#39;root&#39;, it refers to the root directory (workflow group).
+         * @param {string} scheduleId The ID of the schedule to delete
+         * @param {*} [options] Override http request option.
+         * @throws {RequiredError}
+         */
+        deleteSchedulesForWorkflow(
+        	params: { spaceId: string,  spaceProviderId: string,  itemId: string,  scheduleId: string  }
+        ): Promise<Response> {
+           const defaultParams = { 
+           }
+
+           return rpcClient.call('SpaceService.deleteSchedulesForWorkflow', { ...defaultParams, ...params });
+        },
+        /**
          * Mainly returns the spaces provided by this space-provider.
          * @param {string} spaceProviderId Identifies a space-provider.
          * @param {*} [options] Override http request option.
