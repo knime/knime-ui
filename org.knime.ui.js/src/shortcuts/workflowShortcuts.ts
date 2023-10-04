98ae940964b7cf8826b47fe53074e3df97eb3f72
Benjamin Moser <benjamin.moser@knime.com>
2023-10-04 10:30:08 +0200

    NXT-2062: Re-enable save-back
    NXT-2062 (Space explorer should navigate to location of current workflow if the workflow is opened via the space explorer)

--- org.knime.ui.js/src/shortcuts/workflowShortcuts.ts
+++ org.knime.ui.js/src/shortcuts/workflowShortcuts.ts
@@ -24,7 +24,6 @@ import type { XY } from "@/api/gateway-api/generated-api";
 import { geometry } from "@/util/geometry";
 import { APP_ROUTES } from "@/router/appRoutes";
 import { isNodeMetaNode } from "@/util/nodeUtil";
-import { WorkflowInfo } from "@/api/gateway-api/generated-api";
 
 type WorkflowShortcuts = UnionToShortcutRegistry<
   | "save"
@@ -100,10 +99,7 @@ const workflowShortcuts: WorkflowShortcuts = {
     hotkey: ["Ctrl", "S"],
     icon: SaveIcon,
     execute: ({ $store }) => $store.dispatch("workflow/saveWorkflow"),
-    condition: ({ $store }) =>
-      $store.state.workflow.activeWorkflow?.dirty &&
-      $store.state.workflow.activeWorkflow?.info.providerType ===
-        WorkflowInfo.ProviderTypeEnum.LOCAL,
+    condition: ({ $store }) => $store.state.workflow.activeWorkflow?.dirty,
   },
   saveAs: {
     title: "Save workflow as",
