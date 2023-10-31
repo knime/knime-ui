58864e42a30ba1573cae7eed793612de03c017f2
Fabian Kübler <fabian.kuebler@knime.com>
2023-10-31 18:50:30 +0100

    AP-21394: add keyboard shortcuts
    AP-21394 (Revise K-AI UI/UX)

--- org.knime.ui.js/src/shortcuts/miscShortcuts.ts
+++ org.knime.ui.js/src/shortcuts/miscShortcuts.ts
@@ -1,8 +1,11 @@
 import type { UnionToShortcutRegistry } from "./types";
+import { TABS } from "@/store/panel";
 
 type SelectionShortcuts = UnionToShortcutRegistry<"selectAll" | "deselectAll">;
 
-type SidePanelShortcuts = UnionToShortcutRegistry<"toggleSidePanel">;
+type SidePanelShortcuts = UnionToShortcutRegistry<
+  "toggleSidePanel" | "toggleKai"
+>;
 
 declare module "./index" {
   interface ShortcutsRegistry extends SelectionShortcuts, SidePanelShortcuts {}
@@ -23,5 +26,10 @@ export const sidePanelShortcuts: SidePanelShortcuts = {
   toggleSidePanel: {
     hotkey: ["Ctrl", "P"],
     execute: ({ $store }) => $store.commit("panel/toggleExpanded"),
+  },
+  toggleKai: {
+    hotkey: ["Ctrl", "Alt", "A"],
+    execute: ({ $store }) =>
+      $store.dispatch("panel/setCurrentProjectActiveTab", TABS.AI_CHAT),
   },
 };
