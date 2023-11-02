0329f1460998661c0b5552e34338db7772334a49
Fabian Kübler <fabian.kuebler@knime.com>
2023-11-02 11:24:18 +0100

    AP-21219: fix formatting
    AP-21219 (Deep dive: AI -> Custom Python -> Workflow)

--- org.knime.ui.js/src/store/aiAssistant.ts
+++ org.knime.ui.js/src/store/aiAssistant.ts
@@ -106,7 +106,9 @@ export const actions: ActionTree<AiAssistantState, RootStoreState> = {
   },
   async makeAiRequest({ commit, state, rootGetters }, { chainType, message }) {
     const projectAndWorkflowIds = rootGetters["workflow/projectAndWorkflowIds"];
-    const selectedNodes = rootGetters["selection/selectedNodes"].map(({ id }) => id);
+    const selectedNodes = rootGetters["selection/selectedNodes"].map(
+      ({ id }) => id,
+    );
 
     commit("setIsProcessing", { chainType, isProcessing: true });
     commit("setProjectAndWorkflowIds", { chainType, projectAndWorkflowIds });
