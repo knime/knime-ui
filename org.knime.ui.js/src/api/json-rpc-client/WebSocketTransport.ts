529f9b22310d97dc4db577de38ac7070a97c7189
Martin Horn <martin.horn@knime.com>
2023-12-13 15:56:10 +0100

    NXT-2259: Remove 'handshake' to transmit project-id to backend
    The backend now gets it from the websocket-proxy (via the websocket
    headers).
    
    NXT-2259 (Get job-id from websocket headers)

--- org.knime.ui.js/src/api/json-rpc-client/WebSocketTransport.ts
+++ org.knime.ui.js/src/api/json-rpc-client/WebSocketTransport.ts
@@ -11,61 +11,6 @@ export class WebSocketTransport extends BaseWebSocketTransport {
     this.jobId = jobId;
   }
 
-  public connect(): Promise<any> {
-    const parentConnectPromise = super.connect();
-
-    const handshakePromise = new Promise((resolve, reject) => {
-      // immediately resolve for local development, because the jobId is just a
-      // local workflow instead of a remote job running in the executor
-      if (!this.jobId) {
-        consola.log("Handshake not needed. Skipping");
-        resolve("Skipping handshake");
-        return;
-      }
-
-      // setup handler to receive the handshake. This will only fire upon receiving the
-      // first message on the WS channel, and then the handler will automatically unregister
-      const handshakeResolve = (message) => {
-        const { data } = message;
-        if (typeof data !== "string") {
-          return;
-        }
-
-        try {
-          const parsed = JSON.parse(data);
-
-          if (this.jobId !== null && parsed.status === "success") {
-            this.connection.removeEventListener("message", handshakeResolve);
-            resolve("Handshake success");
-          }
-        } catch (error) {
-          this.connection.removeEventListener("message", handshakeResolve);
-          reject(new Error("Handshake error"));
-        }
-      };
-
-      this.connection.addEventListener("message", handshakeResolve);
-
-      // setup the start of the handshake
-      // as soon as the connection opens a message will be sent over the WS
-      // channel to send the projectId and initiate the handshake
-      const handshakeStart = () => {
-        this.connection.send(
-          JSON.stringify({
-            workflowProjectId: this.jobId,
-          }),
-        );
-
-        // immediately remove the handler since it's not needed after this
-        this.connection.removeEventListener("open", handshakeStart);
-      };
-
-      this.connection.addEventListener("open", handshakeStart);
-    });
-
-    return Promise.all([parentConnectPromise, handshakePromise]);
-  }
-
   public sendData(
     data: JSONRPCRequestData,
     timeout: number | null = DEFAULT_TIMEOUT,
