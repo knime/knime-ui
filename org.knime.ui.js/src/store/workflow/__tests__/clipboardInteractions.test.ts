69adcc94eef71050b627c758d3f0f210b26c055f
Karl Glatz <karl@glatz.biz>
2024-01-18 10:03:34 +0100

    NXT-1176: add tests for clipboard fallback interactions
    NXT-1176 (Make Copy work with Webkit browsers)

--- org.knime.ui.js/src/store/workflow/__tests__/clipboardInteractions.test.ts
+++ org.knime.ui.js/src/store/workflow/__tests__/clipboardInteractions.test.ts
@@ -5,6 +5,7 @@ import {
   it,
   vi,
   type MockedFunction,
+  type Mock,
 } from "vitest";
 import { nextTick } from "vue";
 
@@ -16,6 +17,16 @@ import { loadStore } from "./loadStore";
 import { createConnection } from "@/test/factories";
 
 vi.mock("@/util/pasteToWorkflow");
+
+const show: Mock = vi.fn();
+const remove: Mock = vi.fn();
+const removeBy: Mock = vi.fn();
+
+vi.mock("@/plugins/toasts", () => ({
+  getToastsProvider: () => {
+    return { show, remove, removeBy };
+  },
+}));
 
 const mockedAPI = deepMocked(API);
 const mockedPastePartsAt = pastePartsAt as MockedFunction<typeof pastePartsAt>;
@@ -41,9 +52,9 @@ describe("workflow::clipboardInteractions", () => {
 
     Object.assign(navigator, {
       clipboard: {
-        writeText: (text) => {
+        writeText: vi.fn((text) => {
           clipboardMock.setContent(JSON.parse(text));
-        },
+        }),
         readText: () => {
           const content = clipboardMock.getContent();
           return content ? JSON.stringify(content) : "";
@@ -146,8 +157,103 @@ describe("workflow::clipboardInteractions", () => {
     },
   );
 
+  it("executes alternative copy method for webkit browsers", async () => {
+    const stringifiedPayload = JSON.stringify({
+      payloadIdentifier: "p-id-1",
+      otherData: "is here",
+    });
+
+    mockedAPI.workflowCommand.Copy.mockResolvedValue({
+      content: stringifiedPayload,
+    });
+
+    const clipboardMock = createClipboardMock();
+    navigator.clipboard.writeText
+      .mockImplementationOnce((text) => {
+        clipboardMock.setContent(JSON.parse(text));
+      })
+      .mockImplementationOnce(() => {
+        const error = new Error();
+        error.name = "NotAllowedError";
+        throw error;
+      });
+
+    const { store } = await loadStore();
+
+    store.commit("workflow/setActiveWorkflow", {
+      projectId: "my project",
+      info: { containerId: "root" },
+      nodes: {
+        foo: {
+          id: "foo",
+          position: { x: 0, y: 0 },
+        },
+        bar: {
+          id: "bar",
+          position: { x: 50, y: 50 },
+        },
+      },
+      connections: {
+        connection1: createConnection({
+          bendpoints: [
+            { x: 10, y: 10 },
+            { x: 20, y: 20 },
+          ],
+        }),
+        connection2: createConnection({
+          bendpoints: [{ x: 10, y: 10 }],
+        }),
+      },
+      workflowAnnotations: [
+        {
+          id: "root:2_1",
+          text: "Test",
+          bounds: { x: 10, y: 10, height: 10, width: 10 },
+        },
+        {
+          id: "root:2_2",
+          text: "Test1",
+          bounds: { x: 20, y: 20, height: 20, width: 20 },
+        },
+      ],
+    });
+
+    await store.dispatch("selection/selectAllObjects");
+    await store.dispatch("selection/selectBendpoints", [
+      "connection1__0",
+      "connection1__1",
+      "connection2__0",
+    ]);
+    await nextTick();
+    await store.dispatch("workflow/copyOrCutWorkflowParts", {
+      command: "copy",
+    });
+
+    expect(mockedAPI.workflowCommand.Copy).toHaveBeenCalledWith({
+      projectId: "my project",
+      workflowId: "root",
+      nodeIds: ["foo", "bar"],
+      annotationIds: ["root:2_1", "root:2_2"],
+      connectionBendpoints: {
+        connection1: [0, 1],
+        connection2: [0],
+      },
+    });
+
+    expect(clipboardMock.getContent()).toStrictEqual({
+      cacheClipboardContentId: "clipboard_cache_3",
+    });
+
+    // toast
+    expect(show).toBeCalledWith(
+      expect.objectContaining({ id: "COPY_FALLBACK" }),
+    );
+  });
+
   describe("executes paste command", () => {
-    const setupStoreForPaste = async () => {
+    const setupStoreForPaste = async (
+      cacheClipboardContentId: string | null = null,
+    ) => {
       // register "pasteWorkflowParts" API function
       mockedAPI.workflowCommand.Paste.mockReturnValue({
         nodeIds: ["bar"],
@@ -166,13 +272,17 @@ describe("workflow::clipboardInteractions", () => {
       store.commit("workflow/setActiveWorkflow", workflow);
 
       // mock current clipboard content
-      const clipboardMock = createClipboardMock({
-        objectBounds: {
-          width: 100,
-          height: 100,
-        },
-        data: "parts",
-      });
+      const clipboardMock = createClipboardMock(
+        cacheClipboardContentId
+          ? { cacheClipboardContentId }
+          : {
+              objectBounds: {
+                width: 100,
+                height: 100,
+              },
+              data: "parts",
+            },
+      );
 
       // mock strategy result
       const doAfterPasteMock = vi.fn();
@@ -213,6 +323,31 @@ describe("workflow::clipboardInteractions", () => {
         clipboardContent: clipboardMock.getContent(),
         isWorkflowEmpty: false,
         dispatch: expect.any(Function),
+      });
+    });
+
+    it("uses clipboard cache if content has cacheId", async () => {
+      const cacheClipboardContentId = "clipboard_id_1";
+      const { startPaste, store } = await setupStoreForPaste(
+        cacheClipboardContentId,
+      );
+      store.commit("workflow/setClipboardContentCache", {
+        cacheClipboardContentId,
+        clipboardContent: JSON.stringify({
+          objectBounds: {
+            width: 100,
+            height: 100,
+          },
+          data: "cached_parts",
+        }),
+      });
+      await startPaste();
+
+      expect(mockedAPI.workflowCommand.Paste).toHaveBeenCalledWith({
+        projectId: "my project",
+        workflowId: "root",
+        content: "cached_parts",
+        position: { x: 5, y: 5 },
       });
     });
 
