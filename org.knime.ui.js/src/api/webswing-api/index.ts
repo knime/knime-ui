70bb1c49d65595ac83bc582a5a6830b19238c213
Kai Franze <kai.franze@knime.com>
2024-02-26 12:16:32 +0100

    NXT-2267: WIP
    NXT-2267 (Revising Webswing PoC)

--- org.knime.ui.js/src/api/webswing-api/index.ts
+++ org.knime.ui.js/src/api/webswing-api/index.ts
@@ -31,64 +31,14 @@ const openDialog = async ({
   return response;
 };
 
-// const base64codes = [
-//   255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255,
-//   255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255,
-//   255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 62, 255, 255, 255, 63,
-//   52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 255, 255, 255, 0, 255, 255,
-//   255, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14,
-//   15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 255, 255, 255, 255, 255,
-//   255, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40,
-//   41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51
-// ];
-  
-// const getBase64Code = (charCode: any) => {
-//   if (charCode >= base64codes.length) {
-//     throw new Error("Unable to parse base64 string.");
-//   }
-//   const code = base64codes[charCode];
-//   if (code === 255) {
-//     throw new Error("Unable to parse base64 string.");
-//   }
-//   return code;
-// };
-
-// /**
-//  * TODO: Find out if this is correct.
-//  */
-// const base64ToBytes = (str: any) => {
-//   if (str.length % 4 !== 0) {
-//     throw new Error("Unable to parse base64 string.");
-//   }
-//   const index = str.indexOf("=");
-//   if (index !== -1 && index < str.length - 2) {
-//     throw new Error("Unable to parse base64 string.");
-//   }
-//   const missingOctets = str.endsWith("==") ? 2 : (str.endsWith("=") ? 1 : 0);
-//   const n = str.length;
-//   const result = new Uint8Array(3 * (n / 4));
-//   let buffer;
-//   for (let i = 0, j = 0; i < n; i += 4, j += 3) {
-//     buffer =
-//       getBase64Code(str.charCodeAt(i)) << 18 |
-//       getBase64Code(str.charCodeAt(i + 1)) << 12 |
-//       getBase64Code(str.charCodeAt(i + 2)) << 6 |
-//       getBase64Code(str.charCodeAt(i + 3));
-//     result[j] = buffer >> 16;
-//     result[j + 1] = (buffer >> 8) & 0xFF;
-//     result[j + 2] = buffer & 0xFF;
-//   }
-//   return result.subarray(0, result.length - missingOctets);
-// };
-
-/**
- * TODO: Find out if this is correct.
- */
-const base64ToBytes = (base64: string) => {
-  const binString = atob(base64);
-  const array = Uint8Array.from(binString, (m) => m.codePointAt(0)); // eslint-disable-line
-  return new TextDecoder().decode(array);
-}
+const base64ToBytes = (str: any) => {
+  const binaryString = atob(str);
+  const bytes = new Uint8Array(binaryString.length);
+  for (let i = 0; i < binaryString.length; i++) {
+      bytes[i] = binaryString.charCodeAt(i);
+  }
+  return bytes.buffer;
+};
 
 /**
  * Sets the binary message consumer
@@ -106,6 +56,7 @@ const applyMessageConsumer = (message: string) => {
   }
   if (messageConsumer) {
     const bytes = base64ToBytes(message);
+    // debugger;
     messageConsumer(bytes);  
   }
 }
