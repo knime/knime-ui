c6a38324f87780d0ab9352189e047d7c1d1731f6
Benjamin Moser <benjamin.moser@knime.com>
2024-02-29 14:25:20 +0100

    AP-2431: Code style improvements as indicated by SonarLint (no functional changes)
    AP-2431 (Java Snippet: Show line numbers in compile errors)

--- org.knime.ui.java/src/eclipse/org/knime/ui/java/browser/KnimeBrowserView.java
+++ org.knime.ui.java/src/eclipse/org/knime/ui/java/browser/KnimeBrowserView.java
@@ -1,3 +1,48 @@
+/*
+ * ------------------------------------------------------------------------
+ *
+ *  Copyright by KNIME AG, Zurich, Switzerland
+ *  Website: http://www.knime.com; Email: contact@knime.com
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License, Version 3, as
+ *  published by the Free Software Foundation.
+ *
+ *  This program is distributed in the hope that it will be useful, but
+ *  WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, see <http://www.gnu.org/licenses>.
+ *
+ *  Additional permission under GNU GPL version 3 section 7:
+ *
+ *  KNIME interoperates with ECLIPSE solely via ECLIPSE's plug-in APIs.
+ *  Hence, KNIME and ECLIPSE are both independent programs and are not
+ *  derived from each other. Should, however, the interpretation of the
+ *  GNU GPL Version 3 ("License") under any applicable laws result in
+ *  KNIME and ECLIPSE being a combined program, KNIME AG herewith grants
+ *  you the additional permission to use and propagate KNIME together with
+ *  ECLIPSE with only the license terms in place for ECLIPSE applying to
+ *  ECLIPSE and the GNU GPL Version 3 applying for KNIME, provided the
+ *  license terms of ECLIPSE themselves allow for the respective use and
+ *  propagation of ECLIPSE together with KNIME.
+ *
+ *  Additional permission relating to nodes for KNIME that extend the Node
+ *  Extension (and in particular that are based on subclasses of NodeModel,
+ *  NodeDialog, and NodeView) and that only interoperate with KNIME through
+ *  standard APIs ("Nodes"):
+ *  Nodes are deemed to be separate and independent programs and to not be
+ *  covered works.  Notwithstanding anything to the contrary in the
+ *  License, the License does not apply to Nodes, you are not required to
+ *  license Nodes under the License, and you are granted a license to
+ *  prepare and propagate Nodes, in each case even if such Nodes are
+ *  propagated with or for interoperation with KNIME.  The owner of a Node
+ *  may freely choose the license terms applicable to such Node, including
+ *  when such Node is propagated with or for interoperation with KNIME.
+ * ---------------------------------------------------------------------
+ */
 package org.knime.ui.java.browser;
 
 import static org.knime.ui.java.util.PerspectiveUtil.BROWSER_VIEW_PART_ID;
@@ -53,11 +98,11 @@ public class KnimeBrowserView {
 
     private static final String APP_PAGE = BASE_URL + "/index.html";
 
-    private static Runnable viewInitializer = null;
+    private static Runnable viewInitializer;
 
     private static Browser browser;
 
-    static boolean isInitialized = false;
+    static boolean isInitialized;
 
     /**
      * Activates the view initializer that will be executed as soon as this view becomes finally visible (again). Once
@@ -65,7 +110,7 @@ public class KnimeBrowserView {
      *
      * @param checkForUpdates whether to check for updates on initialization
      */
-    public static void activateViewInitializer(final boolean checkForUpdates) {
+    public static synchronized void activateViewInitializer(final boolean checkForUpdates) {
         viewInitializer = () -> initView(false, checkForUpdates);
     }
 
