8816fc0678134a0ceec73df6c2309694b9f18958
Karl Glatz <karl@glatz.biz>
2024-04-08 08:56:12 +0200

    NXT-2531: remove splitpanes as dep
    NXT-2531 (Investigate pixel based side panel)

--- org.knime.ui.js/.eslintrc.cjs
+++ org.knime.ui.js/.eslintrc.cjs
@@ -18,15 +18,6 @@ module.exports = {
       },
     },
   },
-  overrides: [
-    // TODO: turn this on again after fixing splitpanes import problems
-    {
-      files: ["**/SplitPanel.vue"],
-      rules: {
-        "import/extensions": ["off"],
-      },
-    },
-  ],
   rules: {
     "new-cap": ["warn", { capIsNewExceptionPattern: "^API\\.." }],
   },
