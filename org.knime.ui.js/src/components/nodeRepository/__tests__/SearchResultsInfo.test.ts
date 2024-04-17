3f12efce7267a1a8770c3c06d0c978634ac08e3d
Juan Jose Diaz Baquero <juan.diazbaquero@tngtech.com>
2024-04-17 16:42:43 +0200

    NXT-2473: Add test for search results info on empty results in the browser
    NXT-2473 (Adjust node repository presentation for playground)

--- org.knime.ui.js/src/components/nodeRepository/__tests__/SearchResultsInfo.test.ts
+++ org.knime.ui.js/src/components/nodeRepository/__tests__/SearchResultsInfo.test.ts
@@ -22,6 +22,7 @@ describe("SearchResultsInfo", () => {
       isNodeListEmpty: false,
       showDownloadButton: false,
       searchHubLink: "",
+      isQuickAddNodeMenu: false,
       ...propsOverrides,
     };
 
@@ -38,60 +39,92 @@ describe("SearchResultsInfo", () => {
   };
 
   describe("searchResultsInfo", () => {
-    it("shows placeholder for empty result if numFilteredOutNodes is a positive value", async () => {
-      const { wrapper } = doMount();
+    describe("has filtered out nodes", () => {
+      it("shows placeholder for empty result if numFilteredOutNodes is a positive value", async () => {
+        const { wrapper } = doMount();
 
-      expect(wrapper.text()).toMatch(
-        "Change filter settings to “All nodes“ to see more advanced nodes matching your search criteria.",
-      );
-      await wrapper.findComponent(FilterCheckIcon).trigger("click");
-      expect(mockedAPI.desktop.openWebUIPreferencePage).toHaveBeenCalled();
+        expect(wrapper.text()).toMatch(
+          "Change filter settings to “All nodes“ to see more advanced nodes matching your search criteria.",
+        );
+        await wrapper.findComponent(FilterCheckIcon).trigger("click");
+        expect(mockedAPI.desktop.openWebUIPreferencePage).toHaveBeenCalled();
+      });
+
+      it("shows download AP button and message for an empty search result in the browser", async () => {
+        vi.resetModules();
+        vi.doMock("@/environment", async (importOriginal) => {
+          const actual = await importOriginal<typeof import("@/environment")>();
+
+          return {
+            ...actual,
+            environment: "BROWSER",
+            isDesktop: false,
+            isBrowser: true,
+          };
+        });
+        const SearchResultsInfo = (await import("../SearchResultsInfo.vue"))
+          .default;
+
+        const { wrapper } = doMount({
+          propsOverrides: {
+            showDownloadButton: true,
+          },
+          component: SearchResultsInfo,
+        });
+
+        expect(wrapper.find("download-a-p-button-stub").exists()).toBe(true);
+        expect(wrapper.text()).toMatch(
+          "There are no available matching nodes. To work with more nodes, download the KNIME Analytics Platform.",
+        );
+      });
     });
 
-    it("shows download AP button and message for an empty search result in the browser", async () => {
-      vi.resetModules();
-      vi.doMock("@/environment", async (importOriginal) => {
-        const actual = await importOriginal<typeof import("@/environment")>();
+    describe("has empty result", () => {
+      it("shows placeholder for empty result if numFilteredOutNodes is 0", () => {
+        const query = "xxx xxx";
+        const encodedQuery = `https://hub.knime.com/search?q=${encodeURIComponent(
+          query,
+        )}&type=all&src=knimeappmodernui`;
+        const { wrapper } = doMount({
+          propsOverrides: {
+            numFilteredOutNodes: 0,
+            isNodeListEmpty: true,
+            searchHubLink: encodedQuery,
+          },
+        });
 
-        return {
-          ...actual,
-          environment: "BROWSER",
-          isDesktop: false,
-          isBrowser: true,
-        };
-      });
-      const SearchResultsInfo = (await import("../SearchResultsInfo.vue"))
-        .default;
-
-      const { wrapper } = doMount({
-        propsOverrides: {
-          showDownloadButton: true,
-        },
-        component: SearchResultsInfo,
+        expect(wrapper.text()).toMatch("There are no matching nodes.");
+        expect(wrapper.text()).toMatch("Search the KNIME Community Hub");
+        expect(wrapper.find("a").attributes("href")).toBe(encodedQuery);
       });
 
-      expect(wrapper.find("download-a-p-button-stub").exists()).toBe(true);
-      expect(wrapper.text()).toMatch(
-        "There are no available matching nodes. To work with more nodes, download the KNIME Analytics Platform.",
-      );
-    });
+      it("shows download button in browser", async () => {
+        vi.resetModules();
+        vi.doMock("@/environment", async (importOriginal) => {
+          const actual = await importOriginal<typeof import("@/environment")>();
 
-    it("shows placeholder for empty result if numFilteredOutNodes is 0", () => {
-      const query = "xxx xxx";
-      const encodedQuery = `https://hub.knime.com/search?q=${encodeURIComponent(
-        query,
-      )}&type=all&src=knimeappmodernui`;
-      const { wrapper } = doMount({
-        propsOverrides: {
-          numFilteredOutNodes: 0,
-          isNodeListEmpty: true,
-          searchHubLink: encodedQuery,
-        },
+          return {
+            ...actual,
+            environment: "BROWSER",
+            isDesktop: false,
+            isBrowser: true,
+          };
+        });
+        const SearchResultsInfo = (await import("../SearchResultsInfo.vue"))
+          .default;
+
+        const { wrapper } = doMount({
+          propsOverrides: {
+            numFilteredOutNodes: 0,
+            isNodeListEmpty: true,
+          },
+          component: SearchResultsInfo,
+        });
+
+        expect(wrapper.text()).toMatch(
+          "There are more open source extensions and nodes available in the full version of the KNIME Analytics Platform.",
+        );
       });
-
-      expect(wrapper.text()).toMatch("There are no matching nodes.");
-      expect(wrapper.text()).toMatch("Search the KNIME Community Hub");
-      expect(wrapper.find("a").attributes("href")).toBe(encodedQuery);
     });
   });
 });
