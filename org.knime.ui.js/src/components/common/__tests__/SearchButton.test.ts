1fbe4e989f8bab5a17abae5d14b421d5db543979
Karl Glatz <karl@glatz.biz>
2024-06-10 12:59:24 +0200

    NXT-2666: add test for SearchButton
    NXT-2666 (Filter current space explorer level with a search input)

---
+++ org.knime.ui.js/src/components/common/__tests__/SearchButton.test.ts
@@ -0,0 +1,48 @@
+import { describe, expect, it } from "vitest";
+import { mount } from "@vue/test-utils";
+
+import SearchButton from "../SearchButton.vue";
+import FunctionButton from "webapps-common/ui/components/FunctionButton.vue";
+import InputField from "webapps-common/ui/components/forms/InputField.vue";
+import { nextTick } from "vue";
+
+describe("Search Button", () => {
+  const doMount = () => {
+    const wrapper = mount(SearchButton, {
+      props: {
+        modelValue: "Query",
+      },
+    });
+
+    return { wrapper };
+  };
+
+  it("renders closed", () => {
+    const { wrapper } = doMount();
+    expect(wrapper.findComponent(FunctionButton).exists()).toBeTruthy();
+    expect(wrapper.findComponent(InputField).exists()).toBeFalsy();
+  });
+
+  it("opens on click", async () => {
+    const { wrapper } = doMount();
+    wrapper.findComponent(FunctionButton).vm.$emit("click");
+    await nextTick();
+    expect(wrapper.findComponent(InputField).exists).toBeTruthy();
+    expect(wrapper.findComponent(InputField).props("modelValue")).toBe("Query");
+  });
+
+  it("clears and closes on click if open", async () => {
+    const { wrapper } = doMount();
+    wrapper.findComponent(FunctionButton).vm.$emit("click");
+    await nextTick();
+    expect(wrapper.findComponent(InputField).exists).toBeTruthy();
+    expect(wrapper.findComponent(InputField).props("modelValue")).toBe("Query");
+    wrapper.findComponent(FunctionButton).vm.$emit("click");
+    await nextTick();
+    // closed state
+    expect(wrapper.findComponent(FunctionButton).exists()).toBeTruthy();
+    expect(wrapper.findComponent(InputField).exists()).toBeFalsy();
+    // clears search on close
+    expect(wrapper.emitted("update:modelValue")?.[0]?.[0]).toBe("");
+  });
+});
