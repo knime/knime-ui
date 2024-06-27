748c79a566ac500d57bdcea51f1941b6ea6e47cf
Juan Jose Diaz Baquero <juan.diazbaquero@tngtech.com>
2024-06-27 14:21:57 +0200

    NXT-2696: add NameCollisionContext type
    NXT-2696 (Moving space item(s) is not working anymore)

--- org.knime.ui.js/src/api/desktop-api/desktop-api.ts
+++ org.knime.ui.js/src/api/desktop-api/desktop-api.ts
@@ -483,7 +483,7 @@ export const getNameCollisionStrategy = ({
   SpaceId & {
     itemIds: string[];
     destinationItemId: string;
-    usageContext: string;
+    usageContext: "IMPORT" | "SAVE" | "COPY" | "MOVE";
   }) => {
   return callBrowserFunction(
     window.getNameCollisionStrategy,
