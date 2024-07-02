60dfcc17fcfe18ac64909fd297012e2a1d2a5c10
Gosia Wojcik <gosia.wojcik@knime.com>
2024-07-02 13:50:07 +0200

    NXT-2710: Escape special characters in the query
    NXT-2710 (Starting with forbidden character (e.g, * or ?) breaks the space explorer search)

--- org.knime.ui.js/src/util/matchesQuery.ts
+++ org.knime.ui.js/src/util/matchesQuery.ts
@@ -1,7 +1,7 @@
 export const matchesQuery = (query: string, input: string) => {
-  // Replace forbidden character only at the beginning of the query
-  const forbiddenCharacters = /^[*?#:";<>%~|/\\]+/;
-  const cleanedQuery = query.replace(forbiddenCharacters, "");
+  // Escape special characters in the list
+  const charactersToEscape = /[.*+$^?#:";<>()%~|/\\[\]]/g;
+  const cleanedQuery = query.replace(charactersToEscape, "\\$&");
 
   return new RegExp(cleanedQuery, "i").test(input);
 };
