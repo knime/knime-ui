df06ca0cfa0dc35e18ccadfae46a11e12b84c741
Helian Rivera <helian.rivera@knime.com>
2024-07-23 13:19:10 +0200

    NXT-2803: add button to reload spaces page
    NXT-2803 (Add option to refresh spaces in the Space Selection page (cards))

--- org.knime.ui.js/src/components/spaces/SpaceSelectionPage.vue
+++ org.knime.ui.js/src/components/spaces/SpaceSelectionPage.vue
@@ -2,6 +2,9 @@
 import { computed, ref } from "vue";
 import { useRouter } from "vue-router";
 
+import { FunctionButton } from "@knime/components";
+import ReloadIcon from "@knime/styles/img/icons/reload.svg";
+
 import { APP_ROUTES } from "@/router/appRoutes";
 import { useStore } from "@/composables/useStore";
 import type { SpaceProviderNS } from "@/api/custom-types";
@@ -14,20 +17,27 @@ import SpaceExplorerFloatingButton from "./SpaceExplorerFloatingButton.vue";
 import { useActiveRouteData } from "./useActiveRouteData";
 import { usePageBreadcrumbs } from "./usePageBreadcrumbs";
 import { useSpaceIcons } from "./useSpaceIcons";
+import SkeletonItem from "@/components/common/skeleton-loader/SkeletonItem.vue";
 import SearchButton from "@/components/common/SearchButton.vue";
 import { matchesQuery } from "@/util/matchesQuery";
 import { formatSpaceProviderName } from "./formatSpaceProviderName";
+import { getToastsProvider } from "@/plugins/toasts";
 
 type SpaceWithGroupId = SpaceProviderNS.Space & { groupId: string };
 
 const $router = useRouter();
 const store = useStore();
+const $toast = getToastsProvider();
 
 const { activeSpaceProvider, activeSpaceGroup, isShowingAllSpaces } =
   useActiveRouteData();
 
 const { breadcrumbs } = usePageBreadcrumbs();
 const { getSpaceGroupIcon, getSpaceProviderIcon } = useSpaceIcons();
+
+const isLoadingProviderSpaces = computed(
+  () => store.state.spaces.isLoadingProviderSpaces,
+);
 
 const onSpaceCardClick = (space: SpaceWithGroupId) => {
   $router.push({
@@ -89,6 +99,27 @@ const createSpace = () => {
     $router,
   });
 };
+
+const reload = async () => {
+  try {
+    await store.dispatch("spaces/reloadProviderSpaces", {
+      id: activeSpaceProvider.value.id,
+    });
+  } catch (error) {
+    const message =
+      error &&
+      typeof error === "object" &&
+      "message" in error &&
+      typeof error.message === "string"
+        ? error.message
+        : "Could not reload spaces";
+
+    $toast.show({
+      type: "error",
+      message,
+    });
+  }
+};
 </script>
 
 <template>
@@ -107,6 +138,9 @@ const createSpace = () => {
 
     <template #toolbar>
       <SearchButton v-model="query" />
+      <FunctionButton class="reload-button" @click="reload">
+        <ReloadIcon />
+      </FunctionButton>
       <SpaceExplorerFloatingButton
         v-if="!isShowingAllSpaces"
         :disabled="isCreateSpaceDisabled"
@@ -117,12 +151,31 @@ const createSpace = () => {
 
     <template #content>
       <div class="cards">
-        <SpaceCard
-          v-for="(space, id) of filteredSpaces"
-          :key="id"
-          :space="space"
-          @click="onSpaceCardClick(space)"
-        />
+        <template v-if="!isLoadingProviderSpaces">
+          <SpaceCard
+            v-for="(space, id) of filteredSpaces"
+            :key="id"
+            :space="space"
+            @click="onSpaceCardClick(space)"
+          />
+        </template>
+
+        <template v-else>
+          <SkeletonItem
+            style="box-shadow: var(--shadow-elevation-1)"
+            height="230px"
+          />
+
+          <SkeletonItem
+            style="box-shadow: var(--shadow-elevation-1)"
+            height="230px"
+          />
+
+          <SkeletonItem
+            style="box-shadow: var(--shadow-elevation-1)"
+            height="230px"
+          />
+        </template>
       </div>
     </template>
   </SpacePageLayout>
@@ -137,5 +190,9 @@ const createSpace = () => {
 
 :deep(.search-button-input) {
   width: 300px;
+}
+
+.reload-button {
+  margin-left: var(--space-4);
 }
 </style>
