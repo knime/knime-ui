9d33491cfc59379d761dfcfea9d8198d91002a11
Philipp Bitzer <philipp.bitzer@tngtech.com>
2024-09-18 17:20:18 +0200

    NXT-2719: Hotkey: Switch to next project tab
    NXT-2719 (Change active tab via hotkey Ctrl + Tab)

--- org.knime.ui.js/src/shortcuts/__tests__/applicationShortcuts.test.js
+++ org.knime.ui.js/src/shortcuts/__tests__/applicationShortcuts.test.js
@@ -4,6 +4,7 @@ import {
   cachedLocalSpaceProjectId,
   globalSpaceBrowserProjectId,
 } from "@/store/spaces";
+import { APP_ROUTES } from "@/router/appRoutes";
 
 describe("applicationShortcuts", () => {
   let mockDispatch, mockCommit, $store;
@@ -88,13 +89,13 @@ describe("applicationShortcuts", () => {
     });
   });
 
-  it("closeProject", () => {
-    applicationShortcuts.closeProject.execute({ $store });
-    expect(mockDispatch).toHaveBeenCalledWith("workflow/closeProject", "1");
-  });
+  describe("closeProject", () => {
+    it("execute", () => {
+      applicationShortcuts.closeProject.execute({ $store });
+      expect(mockDispatch).toHaveBeenCalledWith("workflow/closeProject", "1");
+    });
 
-  describe("condition", () => {
-    it("closeProject", () => {
+    it("condition", () => {
       expect(applicationShortcuts.closeProject.condition({ $store })).toBe(
         true,
       );
@@ -103,5 +104,97 @@ describe("applicationShortcuts", () => {
         applicationShortcuts.closeProject.condition({ $store }),
       ).toBeFalsy();
     });
+  });
+
+  describe("switchTabs", () => {
+    // Shared test for forward/backward conditions
+    it.each([
+      [
+        "switchToNextWorkflow",
+        applicationShortcuts.switchToNextWorkflow.condition,
+      ],
+      [
+        "switchToPreviousWorkflow",
+        applicationShortcuts.switchToPreviousWorkflow.condition,
+      ],
+    ])("condition (%s)", (_, condition) => {
+      $store.state.application = {
+        activeProjectId: "A",
+        openProjects: [
+          { projectId: "A" },
+          { projectId: "B" },
+          { projectId: "C" },
+        ],
+      };
+      expect(condition({ $store })).toBe(true);
+      // no active project
+      $store.state.application = {
+        openProjects: [
+          { projectId: "A" },
+          { projectId: "B" },
+          { projectId: "C" },
+        ],
+      };
+      expect(condition({ $store })).toBe(false);
+      // Only one open project
+      $store.state.application = {
+        activeProjectId: "A",
+        openProjects: [{ projectId: "A" }],
+      };
+      expect(condition({ $store })).toBe(false);
+    });
+
+    it.each([
+      ["A", "B"],
+      ["B", "C"],
+      ["C", "A"],
+    ])(
+      "execute switchToNextWorkflow (%s -> %s)",
+      (activeProjectId, expectedProjectId) => {
+        const $router = { push: vi.fn() };
+        $store.state.application.openProjects = [
+          { projectId: "A" },
+          { projectId: "B" },
+          { projectId: "C" },
+        ];
+        $store.state.application.activeProjectId = activeProjectId;
+        applicationShortcuts.switchToNextWorkflow.execute({ $store, $router });
+        expect($router.push).toHaveBeenNthCalledWith(1, {
+          name: APP_ROUTES.WorkflowPage,
+          params: {
+            projectId: expectedProjectId,
+            workflowId: "root",
+          },
+        });
+      },
+    );
+
+    it.each([
+      ["A", "C"],
+      ["B", "A"],
+      ["C", "B"],
+    ])(
+      "execute switchToPreviousWorkflow (%s -> %s)",
+      (activeProjectId, expectedProjectId) => {
+        const $router = { push: vi.fn() };
+        $store.state.application.openProjects = [
+          { projectId: "A" },
+          { projectId: "B" },
+          { projectId: "C" },
+        ];
+        $store.state.application.activeProjectId = activeProjectId;
+        applicationShortcuts.switchToPreviousWorkflow.execute({
+          $store,
+          $router,
+        });
+        expect($router.push).toHaveBeenNthCalledWith(1, {
+          name: APP_ROUTES.WorkflowPage,
+          params: {
+            projectId: expectedProjectId,
+            workflowId: "root",
+          },
+        });
+      },
+    );
   });
 });
--- org.knime.ui.js/src/shortcuts/applicationShortcuts.ts
+++ org.knime.ui.js/src/shortcuts/applicationShortcuts.ts
@@ -3,14 +3,42 @@ import {
   globalSpaceBrowserProjectId,
 } from "@/store/spaces";
 import type { UnionToShortcutRegistry } from "./types";
+import { APP_ROUTES } from "@/router/appRoutes";
+import type { Router } from "vue-router";
+import type { RootStoreState } from "@/store/types";
+import type { Store } from "vuex";
 
 type ApplicationShortcuts = UnionToShortcutRegistry<
-  "closeProject" | "createWorkflow"
+  | "closeProject"
+  | "createWorkflow"
+  | "switchToNextWorkflow"
+  | "switchToPreviousWorkflow"
 >;
 
 declare module "./index" {
   interface ShortcutsRegistry extends ApplicationShortcuts {}
 }
+
+const switchActiveProject = (
+  store: Store<RootStoreState>,
+  router: Router,
+  offset: -1 | 1,
+) => {
+  const { openProjects, activeProjectId } = store.state.application;
+  const currentIndex = openProjects.findIndex(
+    (p) => p.projectId === activeProjectId,
+  );
+  const nextIndex =
+    (currentIndex + offset + openProjects.length) % openProjects.length;
+
+  router.push({
+    name: APP_ROUTES.WorkflowPage,
+    params: {
+      projectId: openProjects[nextIndex].projectId,
+      workflowId: "root",
+    },
+  });
+};
 
 const applicationShortcuts: ApplicationShortcuts = {
   closeProject: {
@@ -52,6 +80,28 @@ const applicationShortcuts: ApplicationShortcuts = {
       });
     },
     condition: ({ $store }) => !$store.state.spaces.isLoadingContent,
+  },
+  switchToNextWorkflow: {
+    text: "Switch to next opened workflow",
+    hotkey: ["CtrlOrCmd", "Tab"],
+    group: "general",
+    execute: ({ $store, $router }) => {
+      switchActiveProject($store, $router, 1);
+    },
+    condition: ({ $store }) =>
+      Boolean($store.state.application.activeProjectId) &&
+      $store.state.application.openProjects.length >= 2,
+  },
+  switchToPreviousWorkflow: {
+    text: "Switch to previous opened workflow",
+    hotkey: ["CtrlOrCmd", "Shift", "Tab"],
+    group: "general",
+    execute: ({ $store, $router }) => {
+      switchActiveProject($store, $router, -1);
+    },
+    condition: ({ $store }) =>
+      Boolean($store.state.application.activeProjectId) &&
+      $store.state.application.openProjects.length >= 2,
   },
 };
 
