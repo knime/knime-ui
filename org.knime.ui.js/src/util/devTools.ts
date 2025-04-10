54b3eea8caf8a7bf471b42e02e8fd6f1b58dd437
Kai Franze <kai.franze@knime.com>
2025-04-10 11:12:18 +0200

    NXT-3301: Adopt Sonar code style improvement
    NXT-3301 (Rework debug buttons for Chromium remote debugger)

--- org.knime.ui.js/src/util/devTools.ts
+++ org.knime.ui.js/src/util/devTools.ts
@@ -2,7 +2,7 @@ export const reloadApp = () => (window.location.href = "/");
 
 export const openInspector = async () => {
   const remoteDebuggingPort =
-    import.meta.env.KNIME_CEF_REMOTE_DEBUGGING_PORT || "8888";
+    import.meta.env.KNIME_CEF_REMOTE_DEBUGGING_PORT ?? "8888";
   const remoteDebuggingUrl = `http://localhost:${remoteDebuggingPort}`;
   const response = await fetch(`${remoteDebuggingUrl}/json`);
   const targets = await response.json();
