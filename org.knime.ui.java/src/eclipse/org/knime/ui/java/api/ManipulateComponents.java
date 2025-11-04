8b3e767f3ab25be42a37c135661ad0f6c7092fdc
Manuel Hotz <manuel.hotz@knime.com>
2025-11-04 10:00:39 +0100

    AP-25227: Use new ResolverUtil method to fetch ID/path for change Component link type
    AP-25227 (Changing Component link type triggers stale remote content exception)

--- org.knime.ui.java/src/eclipse/org/knime/ui/java/api/ManipulateComponents.java
+++ org.knime.ui.java/src/eclipse/org/knime/ui/java/api/ManipulateComponents.java
@@ -48,8 +48,8 @@
  */
 package org.knime.ui.java.api;
 
+import java.io.IOException;
 import java.net.URI;
-import java.net.URISyntaxException;
 import java.net.URL;
 import java.time.Instant;
 import java.util.ArrayList;
@@ -60,7 +60,7 @@ import java.util.Objects;
 import java.util.Optional;
 import java.util.function.BiPredicate;
 
-import org.eclipse.core.runtime.Path;
+import org.eclipse.core.runtime.NullProgressMonitor;
 import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.jface.window.Window;
 import org.eclipse.swt.SWT;
@@ -80,6 +80,7 @@ import org.knime.core.node.workflow.contextv2.HubSpaceLocationInfo;
 import org.knime.core.ui.util.SWTUtilities;
 import org.knime.core.util.exception.ResourceAccessException;
 import org.knime.core.util.hub.HubItemVersion;
+import org.knime.core.util.pathresolve.ResolverUtil;
 import org.knime.core.util.urlresolve.KnimeUrlResolver;
 import org.knime.core.util.urlresolve.KnimeUrlResolver.IdAndPath;
 import org.knime.core.util.urlresolve.KnimeUrlResolver.KnimeUrlVariant;
@@ -198,9 +199,10 @@ final class ManipulateComponents {
      */
     private static Optional<IdAndPath> translateHubUrl(final URL url) {
         try {
-            return queryHubInfo(url.toURI()).map(loc -> new IdAndPath(loc.getWorkflowItemId(),
-                Path.forPosix(loc.getWorkflowPath()).makeRelative()));
-        } catch (URISyntaxException e) {
+            return ResolverUtil.fetchItemInfo(url, new NullProgressMonitor())
+                    .filter(ii -> ii.id().isPresent()) // Hub only
+                    .map(ii -> new IdAndPath(ii.id().orElseThrow(), ii.path().makeRelative()));
+        } catch (final IOException e) {
             LOGGER.debug(e);
         }
         return Optional.empty();
