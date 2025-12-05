4aba538bf81d629fefd110e98b8ef6ee5f7ab191
Kai Franze <kai.franze@knime.com>
2025-12-05 13:11:56 +0100

    NXT-4224: WIP: Adding workflow sync provider to 'init' phase
    NXT-4224 (Automatically / Manually sync when editing workflows in the browser)

--- org.knime.ui.java/src/eclipse/org/knime/ui/java/browser/lifecycle/Init.java
+++ org.knime.ui.java/src/eclipse/org/knime/ui/java/browser/lifecycle/Init.java
@@ -101,6 +101,7 @@ import org.knime.gateway.impl.webui.spaces.SpaceProvidersManager;
 import org.knime.gateway.impl.webui.spaces.SpaceProvidersManager.Key;
 import org.knime.gateway.impl.webui.spaces.local.LocalSpace;
 import org.knime.gateway.impl.webui.spaces.local.LocalSpaceProvider;
+import org.knime.gateway.impl.webui.syncing.WorkflowSyncerProvider;
 import org.knime.gateway.json.util.ObjectMapperUtil;
 import org.knime.js.cef.CEFPlugin;
 import org.knime.js.cef.commservice.CEFCommService;
@@ -181,6 +182,7 @@ final class Init {
         var selectionEventBus = createSelectionEventBus(eventConsumer);
         NodeCategoryExtensions nodeCategoryExtensions =
             () -> NodeSpecCollectionProvider.getInstance().getCategoryExtensions();
+        var workflowSyncerProvider = WorkflowSyncerProvider.disabled();
 
         MostRecentlyUsedProjects mostRecentlyUsedProjects;
         if (state.mostRecentlyUsedProjects() != null) {
@@ -205,7 +207,8 @@ final class Init {
             codeKaiHandler, //
             nodeCollections, //
             nodeCategoryExtensions, //
-            selectionEventBus);
+            selectionEventBus, //
+            workflowSyncerProvider);
 
         DesktopAPI.injectDependencies( //
             projectManager, //
