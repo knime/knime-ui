c8334aa7233c9bf79ef8b570b429db48919886df
Benjamin Moser <benjamin.moser@knime.com>
2025-12-17 16:32:40 +0100

    required props

--- org.knime.ui.js/src/api/gateway-api/generated-api.ts
+++ org.knime.ui.js/src/api/gateway-api/generated-api.ts
@@ -1253,7 +1253,7 @@ export interface ComponentSearchItem {
      * @type {string}
      * @memberof ComponentSearchItem
      */
-    name?: string;
+    name: string;
     /**
      * The description of the component as given by the component creator
      * @type {string}
@@ -1271,7 +1271,7 @@ export interface ComponentSearchItem {
      * @type {string}
      * @memberof ComponentSearchItem
      */
-    type?: ComponentSearchItem.TypeEnum;
+    type: ComponentSearchItem.TypeEnum;
     /**
      * The components&#39;s input ports.
      * @type {Array<ComponentSearchItemPort>}
