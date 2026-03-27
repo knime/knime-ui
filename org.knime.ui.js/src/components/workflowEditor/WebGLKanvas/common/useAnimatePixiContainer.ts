32d90338ed21960a239314a5517641654c0c315f
Helian Rivera <23222936+hriverahdez@users.noreply.github.com>
2026-03-27 16:19:42 +0100

    NXT-4626: fix animation teardown (#212)
    NXT-4626 (animation issues)

--- org.knime.ui.js/src/components/workflowEditor/WebGLKanvas/common/useAnimatePixiContainer.ts
+++ org.knime.ui.js/src/components/workflowEditor/WebGLKanvas/common/useAnimatePixiContainer.ts
@@ -92,8 +92,11 @@ export const useAnimatePixiContainer = <T extends {}>(options: Options<T>) => {
 
   const doAnimateOut = () => {
     if (!animateOut) {
-      triggerAnimationUpdate(initialValue, "out");
+      // teardown animation before resetting to initial state
+      // otherwise the abrupt change in value can cause an external interaction (e.g pointerover)
+      // to fire off the animation, thus leaving it the value in an intermediate state
       teardown();
+      triggerAnimationUpdate(initialValue, "out");
       return;
     }
 
